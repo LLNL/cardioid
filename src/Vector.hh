b108ff17669f36201b03b7f4b0d8b96578905afe
David Richards <richards12@llnl.gov>
2011-11-04 15:19:26 +0000

    Make most methods of Vector class inline
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/trunk@67 baecf98e-1e81-4516-8920-d4c889e67ea4

--- src/Vector.hh
+++ src/Vector.hh
@@ -2,7 +2,7 @@
 #define VECTOR_HH
 
 #include <iosfwd>
-
+#include <cmath>
 class Vector
 {
  public:
@@ -49,11 +49,146 @@ double length(const Vector& a);
 // input , output functions
 std::ostream& operator<<(std::ostream&, const Vector&);
 
+// Inline Implementations:
+
+inline
+Vector::Vector()
+{
+   _r[0]=0.0;
+   _r[1]=0.0;
+   _r[2]=0.0;
+}
+
+inline
+Vector::Vector(double x, double y, double z)
+{
+   _r[0]=x;
+   _r[1]=y;
+   _r[2]=z;
+}
+
+inline void
+Vector::assign(double x, double y, double z)
+{
+   _r[0] = x;
+   _r[1] = y;
+   _r[2] = z;
+}
+
+inline double
+Vector::length() const
+{
+   return std::sqrt(_r[0]*_r[0] + _r[1]*_r[1] + _r[2]*_r[2]);
+}
+
+inline void
+Vector::normalize()
+{
+   double len = length();
+   if ( len > 0 )
+      *this/=len; 
+}
+   
+inline Vector&
+Vector::operator+=(const Vector& a)
+{
+   this->_r[0] += a._r[0];
+   this->_r[1] += a._r[1];
+   this->_r[2] += a._r[2];
+   return *this;
+}
+inline Vector&
+Vector::operator-=(const Vector& a)
+{
+   this->_r[0] -= a._r[0];
+   this->_r[1] -= a._r[1];
+   this->_r[2] -= a._r[2];
+   return *this;
+}
+inline Vector&
+Vector::operator*=(const double& a)
+{
+   this->_r[0] *= a;
+   this->_r[1] *= a;
+   this->_r[2] *= a;
+   return *this;
+}
+inline Vector&
+Vector::operator/=(const double& a)
+{
+   this->_r[0] /= a;
+   this->_r[1] /= a;
+   this->_r[2] /= a;
+   return *this;
+}
+
+inline Vector
+Vector::operator-() const
+{
+   return (*this) * -1.0;
+}
+
+
+inline bool
+Vector::operator<(const Vector& b) const
+{
+   return
+      _r[0]<b._r[0] ||
+      (_r[0]==b._r[0] &&
+       (_r[1]<b._r[1] ||
+	( _r[1] == b._r[1] && _r[2] < b._r[2] )));
+}
+
+inline Vector
+operator+(const Vector& a, const Vector& b)
+{
+   Vector c(a);
+   return c+=b;
+}
+inline Vector
+operator-(const Vector& a, const Vector& b)
+{
+   Vector c(a);
+   return c-=b;
+}
+inline Vector
+operator*(const Vector& a, const double& b)
+{
+   Vector c(a);
+   return c*=b;
+}
+inline Vector
+operator*(const double& a, const Vector& b)
+{
+   Vector c(b);
+   return c*=a;
+}
+inline Vector
+operator/(const Vector& a, const double& b)
+{
+   Vector c(a);
+   return c/=b;
+}
+inline Vector
+operator/(const double& a, const Vector& b)
+{
+   Vector c(b);
+   return c/=a;
+}
+inline double
+dot(const Vector& a, const Vector& b)
+{
+   return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
+}
+inline double
+length(const Vector& a)
+{
+   return a.length();
+}
+
 inline double diffSq(const Vector& a, const Vector& b)
 {
    return dot(a-b, a-b);
 }
-
-
 
 #endif // ifdef VECTOR_HH
