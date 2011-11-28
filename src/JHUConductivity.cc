122a7a0e521ff3465fa06b733aea3cd8e7bcc026
David Richards <richards12@llnl.gov>
2011-11-28 18:13:31 +0000

    Oops.  Forgot to add the JHUConductivity source files in the last check in.
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/trunk@111 baecf98e-1e81-4516-8920-d4c889e67ea4

---
+++ src/JHUConductivity.cc
@@ -0,0 +1,142 @@
+#include "JHUConductivity.hh"
+#include <cmath>
+#include <cassert>
+#include "AnatomyCell.hh"
+
+using namespace std;
+
+JHUConductivity::JHUConductivity(const JHUConductivityParms& p)
+: homogeneousFiber_(p.homogeneousFiber),
+  rotatationMatrix_(p.rotatationMatrix),
+  GL_(p.sigmaLi),
+  GT_(p.sigmaTi),
+  GN_(p.sigmaTi),
+  sheetAngle_(p.sheetAngle),
+  homogeneousFiberAngle_(-60),
+  endoAngle_(60), // degrees
+  i2t_(p.nx, p.ny, p.nz)
+{
+   double epiAngle = -60; //degrees
+   double totalTwist = endoAngle_ - epiAngle;    // 120 degrees
+   // YDIM is TM axis (in JHU code) defined by max y coord
+   double twistPerCell_ =  totalTwist/p.transmuralAxis;
+}
+
+/** Adapted from GetDiffusionTensorJHU in BlueBeats conductivity.h */
+void JHUConductivity::compute(const AnatomyCell& cell, SigmaTensorMatrix& sigma)
+{
+   //D = R* [G_L 0 0; 0 G_T 0; 0 0 G_N]*R' where R = [E1 E2 E3] = Euler Vecotr Rotation Matrix
+   
+   Tuple globalCoord = i2t_(cell.gid_);
+   double FiberAngle = getFiberAngle(globalCoord);
+   double ImbriAngle = getImbriAngle(globalCoord);
+   double SheetAngle = getSheetAngle(globalCoord);
+   
+   // Convert from Degrees to radians
+   double FibAng = FiberAngle*M_PI/180.;
+   double ImbrAng = ImbriAngle*M_PI/180.;
+   double ShetAng = SheetAngle*M_PI/180.;
+   
+   // !!!!!!!!!!!!!!!
+   // NEEDS MORE WORK - comment by Tabish
+   // !!!!!!!!!!!!!!!!
+   
+   
+   double E1[3];
+   double E3[3];
+   double E2[3];
+   if (rotatationMatrix_ == 0)
+   {
+      // Use Katherine's Rotation Matrix
+      
+      E1[0] = cos(FibAng);               // E_1x
+      E1[1] = 0;                         // E_1y
+      E1[2] = sin(FibAng);               // E_1z
+      E2[0] = -sin(FibAng)*sin(ShetAng); // E_2x
+      E2[1] = cos(ShetAng);              // E_2y
+      E2[2] = cos(FibAng)*sin(ShetAng);  // E_2z
+      E3[0] = sin(FibAng)*cos(ShetAng);  // E_3x
+      E3[1] = sin(ShetAng);              // E_3y
+      E3[2] = -cos(FibAng)*cos(ShetAng); // E_3z
+      
+   }
+   else if (rotatationMatrix_ == 1)
+   {
+      E1[0] = cos(FibAng);               // E_1x
+      E1[1] = 0;                         // E_1y
+      E1[2] = -sin(FibAng);              // E_1z
+      E2[0] = sin(FibAng)*sin(ShetAng);  // E_2x
+      E2[1] = cos(ShetAng);              // E_2y
+      E2[2] = cos(FibAng)*sin(ShetAng);  // E_2z
+      E3[0] = sin(FibAng)*cos(ShetAng);  // E_3x
+      E3[1] = -sin(ShetAng);             // E_3y
+      E3[2] = cos(FibAng)*cos(ShetAng);  // E_3z
+   }
+   else
+      assert(false);
+   
+   // Check: Determinant of Rotation matrix 'E' should be 1
+   double determinant_E =
+      E1[0]*E2[1]*E3[2] + E2[0]*E3[1]*E1[2] + E3[0]*E1[1]*E2[2] -
+      E1[0]*E2[2]*E3[1] - E2[0]*E1[1]*E3[2] - E3[0]*E2[1]*E1[2];
+   
+   double D[3][3];
+   D[0][0] = GL_*pow(E1[0],2) + GT_*pow(E2[0],2) + GN_*pow(E3[0],2);
+   D[1][0] = GL_*E1[0]*E1[1] + GT_*E2[0]*E2[1] + GN_*E3[0]*E3[1];
+   D[2][0] = GL_*E1[0]*E1[2] + GT_*E2[0]*E2[2] + GN_*E3[0]*E3[2];
+   D[0][1] = D[1][0];
+   D[1][1] = GL_*pow(E1[1],2) + GT_*pow(E2[1],2) + GN_*pow(E3[1],2);
+   D[2][1] = GL_*E1[1]*E1[2] + GT_*E2[1]*E2[2] + GN_*E3[1]*E3[2];
+   D[0][2] = D[2][0];
+   D[1][2] = D[2][1];
+   D[2][2] = GL_*pow(E1[2],2) + GT_*pow(E2[2],2) + GN_*pow(E3[2],2);
+   
+   // Check: Determinant of Diffusion Tensor 'D' should be 1
+   double determinant_D =
+      D[0][0]*D[1][1]*D[2][2] + D[0][1]*D[1][2]*D[2][0] + D[0][2]*D[1][0]*D[2][1] -
+      D[0][0]*D[1][2]*D[2][1] - D[0][2]*D[1][1]*D[2][0] - D[0][1]*D[1][0]*D[2][2];
+   
+   // setting the conductivity matrix of the IBM code
+   sigma.a11 = D[0][0];
+   sigma.a12 = D[1][0];
+   sigma.a13 = D[2][0];
+   sigma.a22 = D[1][1];
+   sigma.a23 = D[1][2];
+   sigma.a33 = D[2][2];
+}
+
+
+
+double JHUConductivity::getFiberAngle(const Tuple& globalCoord)
+{
+   double fiberAngle;
+   
+   if (homogeneousFiber_)
+   {
+      // Homogeneous Wedge
+      // Constant fiber angle, i.e. fibers are parallel to each other
+      fiberAngle = homogeneousFiberAngle_;
+   }
+   else
+   {
+      // Transmural wedge
+      // Fibers twist linearly from +60 degrees at Endo
+      // to -60 degrees at Epi. twist angle = 120 degrees
+      // fiber twist = twist angle/fiber length
+      fiberAngle = endoAngle_ - (twistPerCell_ * (globalCoord.y()+1));
+   }
+   
+  return fiberAngle;
+}
+
+
+double JHUConductivity::getImbriAngle(const Tuple&)
+{
+  double ImbrAng = 0;
+  return ImbrAng;
+}
+      
+double JHUConductivity::getSheetAngle(const Tuple&)
+{
+  return sheetAngle_;
+}
---
+++ src/JHUConductivity.hh
@@ -0,0 +1,77 @@
+#ifndef JHU_CONDUCTIVITY_HH
+#define JHU_CONDUCTIVITY_HH
+
+#include "Conductivity.hh"
+#include "IndexToTuple.hh"
+
+// ToDo:
+//
+// 1.  There are probably more parameters that could be exported to the
+// input deck.  These include the homogeneous fiber angle, and the
+// non-homogeneous epiAngle and endoAngle.
+
+
+/** This code is based on functions and compile time macros found in the
+ *  BlueBeats code in conductivity.h and cardiacSetupMPI.h.
+ *
+ *  When BlueBeats.cpp calls GetDiffusionTensorJHU it passes only two
+ *  unique conductivity values: param.sigmaLi and param.sigmaTi.  These
+ *  two parameters are mapped to GL_, GT_, and GN_ in the same way as in
+ *  BlueBeats (GL_ = sigmaLi, GT_ = GN_ = sigmaTi).
+ *
+ *  The function GetDiffusionTensorJHU has two different expressions for
+ *  E1, E2, and E3 depending on the value of the macro ROTATION_MATRIX.
+ *  We reproduce that behavior with a run time parameter.
+ *
+ *  In BlueBeats the getFiberAngleJHU function can return either a
+ *  homogeneous or a non-homogeneous value depending on compile time
+ *  macros.  We default to the non-homogeneous case, but allow a run
+ *  time switch.
+ *
+ *  In BlueBeats the sheetAngle is a compile time parameter.  We choose
+ *  a default of 45 degrees.
+ */
+struct JHUConductivityParms
+{
+   bool homogeneousFiber;
+   int rotatationMatrix;
+   int transmuralAxis;
+
+   // size of Anatomy to initialize IndexToTuple functor.
+   int nx, ny, nz;
+   
+   double sigmaTi;
+   double sigmaLi;
+
+   double sheetAngle;
+};
+
+
+class JHUConductivity : public Conductivity
+{
+ public:
+   JHUConductivity(const JHUConductivityParms& p);
+   void compute(const AnatomyCell& cell, SigmaTensorMatrix& sigma);
+
+ private:
+
+   double getFiberAngle(const Tuple& globalCoord);
+   double getImbriAngle(const Tuple& globalCoord);
+   double getSheetAngle(const Tuple& globalCoord);
+
+   bool homogeneousFiber_;
+   int rotatationMatrix_;
+   
+   double GL_;
+   double GT_;
+   double GN_;
+
+   double sheetAngle_;
+   double homogeneousFiberAngle_;
+   double endoAngle_;
+   double twistPerCell_;
+
+   IndexToTuple i2t_;
+};
+
+#endif
