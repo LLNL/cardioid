9eed35238bbb004375458995d7b9ed233b3aa343
David Richards <richards12@llnl.gov>
2011-12-05 18:04:48 +0000

    Added a not quite finished skeleton to run the benchmark described in the Niederer 2011 paper.  We still need to nail down all the parameters, and the brick structure generator and activationTime sensor are still untested, but this will help us on the way.
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/trunk@125 baecf98e-1e81-4516-8920-d4c889e67ea4

---
+++ test/NiedererTest/tools/mkJobs.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+exe=
+pool=
+maxTime=
+bank=
+nNodes=
+nTasks=
+
+
+for dt in 0.05 0.01 0.005
+do
+for dx in 0.5 0.2 0.1
+do
+for balancer in koradi grid
+do
+
+  dirname=run/${balancer}_dt${dt}_dx${dx}
+  if [ -d $dirname ] 
+  then
+      continue
+  fi
+
+  echo making $dirname
+  mkdir -p $dirname
+
+  case $dx in
+      0.5)
+      stimBox=4
+      ;;
+      0.2)
+      stimBox=8
+      ;;
+      0.1)
+      stimBox=16
+      ;;
+      *)
+      echo ERROR: undefined dx
+      exit -1
+  esac
+
+  case $dt in
+      0.05)
+      maxLoop=20000
+      ;;
+      0.01)
+      stimBox=100000
+      ;;
+      0.005)
+      stimBox=200000
+      ;;
+      *)
+      echo ERROR: undefined dt
+      exit -1
+  esac
+
+
+  cat tools/object.data.proto \
+      | sed s/XX_DT_XX/$dt/ \
+      | sed s/XX_DX_XX/$dx/ \
+      | sed s/XX_MAXLOOP_XX/$maxLoop/ \
+      | sed s/XX_BALANCER_XX/$balancer/ \
+      | sed s/XX_STIM_BOX_XX/$stimBox/ \
+      > $dirname/object.data
+        
+  cat tools/runMe.sh.proto \
+      | sed s%XX_NNODES_XX%$nNodes% \
+      | sed s%XX_NTASKS_XX%$nTasks% \
+      | sed s%XX_MAXTIME_XX%$maxTime% \
+      | sed s%XX_POOL_XX%$pool% \
+      | sed s%XX_BANK_XX%$bank% \
+      | sed s%XX_EXE_XX%$exe% \
+      > $dirname/runMe.sh
+
+done
+done
+done
