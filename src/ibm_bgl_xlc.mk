370c1cace271c5f0cce6c17884d42544889e0fb1
Erik Draeger <draeger1@llnl.gov>
2011-12-14 21:51:27 +0000

    Modified .mk file for BG/L to allow error-free linking.
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/trunk@152 baecf98e-1e81-4516-8920-d4c889e67ea4

--- src/ibm_bgl_xlc.mk
+++ src/ibm_bgl_xlc.mk
@@ -7,12 +7,12 @@
  LD=$(CXX)
 
  DFLAGS += -DWITH_MPI -DADD_ -D$(PLAT) -DUSE_CSTDIO_LFS -DMPICH_IGNORE_CXX_SEEK
- INCLUDE = 
+ INCLUDE = -I/bgl/BlueLight/ppcfloor/bglsys/include
  OPTFLAGS = -g -O3 -qarch=440 -qtune=440 
  CXXFLAGS = $(OPTFLAGS) $(INCLUDE) $(DFLAGS)
- CFLAGS = $(OPTFLAGS) $(INCLUDE) $(DFLAGS)
+ CFLAGS = -qlanglvl=stdc99 $(OPTFLAGS) $(INCLUDE) $(DFLAGS)
 
  LIBPATH = 
- LIBS =
+ LIBS = -lc -lnss_files -lnss_dns -lresolv
 
  LDFLAGS = $(LIBPATH) $(LIBS)
