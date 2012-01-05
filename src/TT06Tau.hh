8b5b10a9d24a3e8ecf986447a253cc7aae7c200e
James Glosli <glosli1@llnl.gov>
2012-01-05 00:06:33 +0000

    TT06Tau.hh: Forget to add with previous check-in
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@191 baecf98e-1e81-4516-8920-d4c889e67ea4

---
+++ src/TT06Tau.hh
@@ -0,0 +1,8 @@
+#ifndef TT06TAU_HH
+#define TT06TAU_HH
+typedef struct { double x0, x1, c[4]; double (*func)(double, double *, double *) ; } TauRecipParms; 
+TauRecipParms* makeTauRecipParms(double V0, double V1, double (*func)(double, double *, double *));
+double jTauRecip(double Vm, double *dtauRecip, double *ddtauRecip);
+double hTauRecip(double Vm, double *dtauRecip, double *ddtauRecip);
+double TauRecipMod(double V, TauRecipParms *parms, double *dR, double *ddR);
+#endif 
