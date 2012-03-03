2e2e776f1949aebf427a62374e0bfbaf4b8f15d8
Matthias Reumann <mre@zurich.ibm.com>
2012-03-03 22:45:40 +0000

    Added tambo configuration with HAVE_GSL = 0
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@386 baecf98e-1e81-4516-8920-d4c889e67ea4

---
+++ src/arch/tambo.mk
@@ -0,0 +1,44 @@
+#-------------------------------------------------------------------------------
+
+CXX=mpixlcxx_r
+CC=mpixlc_r
+LD=$(CXX)
+
+DFLAGS = -DWITH_PIO -DWITH_MPI -DBGP \
+	-DADD_ -DUSE_CSTDIO_LFS -DMPICH_IGNORE_CXX_SEEK
+
+INCLUDE =  -I/bgsys/drivers/ppcfloor/arch/include \
+	-I/usr/local/tools/gsl/include/
+
+
+CFLAGS_BASE =   -qarch=450d $(INCLUDE) $(DFLAGS)
+CXXFLAGS_BASE = -qarch=450d $(INCLUDE) $(DFLAGS)
+#ewd: add floating-point exception traps
+#FPE_TRAP_FLAGS = -qflttrap=enable:nanq:overflow:zerodivide
+CFLAGS_BASE   += $(FPE_TRAP_FLAGS)
+CXXFLAGS_BASE += $(FPE_TRAP_FLAGS)
+OMP_FLAGS = -qsmp=omp 
+
+HAVE_GSL = 0
+ifeq ($(HAVE_GSL),1) 
+   CFLAGS_BASE  += -DHAVE_GSL
+   CXXFLAGS_BASE  += -DHAVE_GSL
+   LDFLAGS_BASE += -L/usr/local/tools/gsl/lib -lgsl -lgslcblas
+endif
+
+CFLAGS_OPT =   $(CFLAGS_BASE) $(OMP_FLAGS) -g -O3 -qtune=450 
+CFLAGS_DEBUG = $(CFLAGS_BASE) $(OMP_FLAGS) -g -O0
+CFLAGS_PROF =  $(CFLAGS_BASE) $(OMP_FLAGS) -g -pg -O3 -DPROFILE
+CFLAGS_MPIP =  $(CFLAGS_BASE) -g -O3 -qtune=450 
+
+CXXFLAGS_OPT =   $(CXXFLAGS_BASE) $(OMP_FLAGS) -g -O3 -qtune=450 
+CXXFLAGS_DEBUG = $(CXXFLAGS_BASE) $(OMP_FLAGS) -g -O0
+CXXFLAGS_PROF =  $(CXXFLAGS_BASE) $(OMP_FLAGS) -g -pg -O3 -DPROFILE
+CXXFLAGS_MPIP =  $(CXXFLAGS_BASE) -g -O3 -qtune=450 
+
+LDFLAGS_OPT   = $(LDFLAGS_BASE) $(CFLAGS_OPT) $(CXXFLAGS_OPT)
+LDFLAGS_DEBUG = $(LDFLAGS_BASE) $(CFLAGS_DEBUG) $(CXXFLAGS_DEBUG)
+LDFLAGS_PROF  = $(LDFLAGS_BASE) $(CFLAGS_PROF) $(CXXFLAGS_PROF)
+
+MPIP_DIR = /usr/local/tools/mpiP/lib
+LDFLAGS_MPIP   =  -L$(MPIP_DIR) -lmpiP -lm $(LDFLAGS_BASE) $(CFLAGS_MPIP) $(CXXFLAGS_MPIP)
