eeebe33ef2d7a68c7c6bde038d6877a91b45eaf3
David Richards <richards12@llnl.gov>
2012-03-09 17:28:26 +0000

    Add a BGQ specific version of the fast barrier code.  Since it doesn't compile (issues with the C++ compiler) it isn't part of the build yet.
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@422 baecf98e-1e81-4516-8920-d4c889e67ea4

--- src/fastBarrier.hh
+++ src/fastBarrier.hh
@@ -1,85 +1,12 @@
 #ifndef FAST_BARRIER_HH
 #define FAST_BARRIER_HH
 
-#include <stdint.h>
-
-struct  L2_Barrier_t
-{
-   volatile  uint64_t start;  /*!< Thread count at start of current round. */
-   volatile  uint64_t count;  /*!< Current thread count. */
-};
-
-
-struct L2_BarrierHandle_t
-{
-  uint64_t localStart; // local (private start)
-  volatile uint64_t *localCountPtr; // encoded fetch and inc address
-};
-
-
-inline void L2_BarrierWithSync_Init(L2_Barrier_t* b)
-{
-   b->start = b->count = 0;
-} 
-
-inline void L2_BarrierWithSync_InitInThread(
-   L2_Barrier_t *b,       /* global barrier */
-   L2_BarrierHandle_t *h) /* barrier handle private to this thread */
-{
-   h->localStart = 0;
-   h->localCountPtr = &b->count;
-}
-
-
-inline void L2_BarrierWithSync_Arrive(
-   L2_Barrier_t *b,        /* global barrier */
-   L2_BarrierHandle_t *h,  /* barrier handle private to this thread */
-   int eventNum)           /* number of arrival events */
-{
-   #pragma omp atomic
-   (*(h->localCountPtr))++;
-//   uint64_t current = count;
-   uint64_t current = *h->localCountPtr;
-   // if reached target, update barrier's start
-   uint64_t target = h->localStart + eventNum;
-   if (current == target)
-   {
-      b->start = current;  // advance to next round
-   }
-}
-
-inline void L2_BarrierWithSync_WaitAndReset(
-   L2_Barrier_t *b,       /* global barrier */
-   L2_BarrierHandle_t *h, /* barrier handle private to this thread */
-   int eventNum)          /* number of arrival events */
-{
-   // compute target from local start
-   uint64_t target = h->localStart + eventNum;
-   // advance local start
-   h->localStart = target;
-   // wait until barrier's start is advanced
-   while (b->start < target) ;
-}
-
-inline void L2_BarrierWithSync_Reset(
-   L2_Barrier_t *b,       /* global barrier */
-   L2_BarrierHandle_t *h, /* barrier handle private to this thread */
-   int eventNum)          /* number of arrival events */
-{
-   // compute target from local start
-   uint64_t target = h->localStart + eventNum;
-   // advance local start
-   h->localStart = target;
-}
-
-inline void L2_BarrierWithSync_Barrier(
-   L2_Barrier_t *b,       /* global barrier */
-   L2_BarrierHandle_t *h, /* barrier handle private to this thread */
-   int eventNum)          /* number of arrival events */
-{
-   L2_BarrierWithSync_Arrive(b, h, eventNum);
-   L2_BarrierWithSync_WaitAndReset(b, h, eventNum);
-}
+// BGQ version of barrier code isn't C++ friednly at the moment.
+// #ifdef BGQ
+// #include "fastBarrierBGQ.hh"
+// #else
+#include "fastBarrierPortable.hh"
+// #endif
 
 
 #endif
