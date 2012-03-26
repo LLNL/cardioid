1a0b6a7fd7f6223a02472501fcdd1cfb4aa4b153
David Richards <richards12@llnl.gov>
2012-03-26 04:23:04 +0000

    PerformanceTimers.cc is dumping core on BGP and the output has some strange lines.  Before I start to work on it I'm just going to clean up the indenting.  There are no changes except indenting in this commit.
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@507 baecf98e-1e81-4516-8920-d4c889e67ea4

--- src/PerformanceTimers.cc
+++ src/PerformanceTimers.cc
@@ -18,22 +18,22 @@
 using namespace std;
 namespace PerformanceTimers
 {
-enum TimerEnum { NCALLS, CYCLES} ;
+   enum TimerEnum { NCALLS, CYCLES} ;
 /*
-#ifdef BGL
-uint64_t getTime()
-{
-   unsigned long long rts_get_timebase(void);
-   uint64_t  t = 0.0;
-   t = rts_get_timebase();
-   return t; 
-}
-double getTick()
-{
-   double seconds_per_cycle = 1.4285714285714285714e-9;
-   return seconds_per_cycle; 
-}
-#endif
+  #ifdef BGL
+  uint64_t getTime()
+  {
+  unsigned long long rts_get_timebase(void);
+  uint64_t  t = 0.0;
+  t = rts_get_timebase();
+  return t; 
+  }
+  double getTick()
+  {
+  double seconds_per_cycle = 1.4285714285714285714e-9;
+  return seconds_per_cycle; 
+  }
+  #endif
 */
 
 #include "PerformanceTimersBGQ.hh"
@@ -183,31 +183,31 @@ void profileStop(const std::string& timerName)
 
 TimerHandle profileGetHandle(string timerName)
 {
-    TimerHandle handle;
-    TimerHandle handleFirst;
-    int id=0; 
+   TimerHandle handle;
+   TimerHandle handleFirst;
+   int id=0; 
     
-    stringstream tmp;
-    tmp <<setfill('0')<<setw(2)<<id<<":"<<timerName;
-    string timerNameID = tmp.str(); 
-    HandleMap::const_iterator iter = handleMap_.find(timerNameID);
-    if (iter != handleMap_.end())
+   stringstream tmp;
+   tmp <<setfill('0')<<setw(2)<<id<<":"<<timerName;
+   string timerNameID = tmp.str(); 
+   HandleMap::const_iterator iter = handleMap_.find(timerNameID);
+   if (iter != handleMap_.end())
       return  iter->second;
-    else
+   else
+   {
+      int nThreads = omp_get_max_threads(); 
+      for (int id =0;id<nThreads;id++)
       {
-	int nThreads = omp_get_max_threads(); 
-	for (int id =0;id<nThreads;id++)
-	  {
-	    stringstream tmp;
-            tmp  <<setfill('0')<<setw(2)<<id<<":"<<timerName;
-	    string timerNameID = tmp.str(); 
-	    handle = timers_.size();
-	    if (id==0) handleFirst=handle; 
-	    handleMap_[timerNameID] = handle;
-	    timers_.push_back(TimerStruct());
-	  }
+         stringstream tmp;
+         tmp  <<setfill('0')<<setw(2)<<id<<":"<<timerName;
+         string timerNameID = tmp.str(); 
+         handle = timers_.size();
+         if (id==0) handleFirst=handle; 
+         handleMap_[timerNameID] = handle;
+         timers_.push_back(TimerStruct());
       }
-    return handleFirst;
+   }
+   return handleFirst;
 }
 
 void profileSetRefTimer(const string& timerName)
@@ -230,25 +230,25 @@ vector<string> generateOutputOrder(const vector<string>& words)
    int nThreads = omp_get_max_threads(); 
    for (int id =0;id<nThreads;id++)
    {
-   stringstream tmp;
-   tmp  <<setfill('0')<<setw(2)<<id<<":";
-   string prefix = tmp.str(); 
-   for (unsigned ii=0; ii<printOrder_.size(); ++ii)
-   {
-      string  name = prefix+printOrder_[ii]; 
-	//printf("name=%s\n",name.c_str()); 
-      if ( printOrder_[ii].empty() || 
-         find(words.begin(), words.end(), name) != words.end() )
+      stringstream tmp;
+      tmp  <<setfill('0')<<setw(2)<<id<<":";
+      string prefix = tmp.str(); 
+      for (unsigned ii=0; ii<printOrder_.size(); ++ii)
       {
-         order.push_back(name);
-         printed.insert(name);
+         string  name = prefix+printOrder_[ii]; 
+         //printf("name=%s\n",name.c_str()); 
+         if ( printOrder_[ii].empty() || 
+              find(words.begin(), words.end(), name) != words.end() )
+         {
+            order.push_back(name);
+            printed.insert(name);
+         }
       }
-   }
-   for (unsigned ii=0; ii<words.size(); ++ii)
-   {
-      if (printed.count(words[ii]) == 0 && prefix == words[ii].substr(0,3))
-         order.push_back(words[ii]);
-   }
+      for (unsigned ii=0; ii<words.size(); ++ii)
+      {
+         if (printed.count(words[ii]) == 0 && prefix == words[ii].substr(0,3))
+            order.push_back(words[ii]);
+      }
    }
    //for (int ii=0;ii<order.size();ii++)  printf("order %d %s\n",ii,order[ii].c_str()); 
    return order;
@@ -258,7 +258,7 @@ vector<string> generateOutputOrder()
    vector<string> words;
    for (HandleMap::const_iterator iter=handleMap_.begin(); iter!=handleMap_.end(); ++iter)
    {
-         words.push_back(iter->first);
+      words.push_back(iter->first);
    }
    
    return generateOutputOrder(words);
@@ -271,7 +271,7 @@ void profileDumpTimes(ostream& out)
    string::size_type maxLen = 0;
    for (HandleMap::iterator iter=handleMap_.begin();
         iter!=handleMap_.end(); ++iter)
-       maxLen = max(maxLen, iter->first.size());
+      maxLen = max(maxLen, iter->first.size());
 
    vector<string> outputOrder = generateOutputOrder();
 
@@ -280,39 +280,39 @@ void profileDumpTimes(ostream& out)
    int counter=CYCLES; 
    {
 
-   out << setw(12) << counterNames_[counter] 
-       << setw(maxLen-5) << " "
-       << setw(10)     << "#Calls" << "  |"
-       << setw(10)      << "Average" 
-       << setw(10)      << "Total"
-       << "   "
-       << setw(8) <<setprecision(2)     << "%Loop" << endl;
-   out << "--------------------------------------------------------------------------------"
-       << endl;
-
-   double refCount = timers_[handleMap_[refTimer_]].total[CYCLES];
-
-   for (unsigned iName=0; iName<outputOrder.size(); ++iName)
-   {
-      const string& name = outputOrder[iName];
-      unsigned ii = handleMap_[name];
-      if (timers_[ii].total[counter]*tick < 0.0001) continue; 
-      if (name.empty())
-      {
-         out << "--------------------------------------------------------------------------------" << endl;
-         continue;
-      }
-      
-      double  ncalls = (double)timers_[ii].total[NCALLS]; 
-      double  count = (double)timers_[ii].total[counter]; 
-      out << setw(maxLen) << left << name << " : "
-          << setw(10) << right << ncalls << "  |"
-          << setw(10) << timers_[ii].total[counter]*tick/ncalls 
-          << setw(10) << timers_[ii].total[counter]*tick 
+      out << setw(12) << counterNames_[counter] 
+          << setw(maxLen-5) << " "
+          << setw(10)     << "#Calls" << "  |"
+          << setw(10)      << "Average" 
+          << setw(10)      << "Total"
           << "   "
-          << setw(8) << setprecision(2) << 100.0*((timers_[ii].total[counter])/refCount)
+          << setw(8) <<setprecision(2)     << "%Loop" << endl;
+      out << "--------------------------------------------------------------------------------"
           << endl;
-   }
+
+      double refCount = timers_[handleMap_[refTimer_]].total[CYCLES];
+
+      for (unsigned iName=0; iName<outputOrder.size(); ++iName)
+      {
+         const string& name = outputOrder[iName];
+         unsigned ii = handleMap_[name];
+         if (timers_[ii].total[counter]*tick < 0.0001) continue; 
+         if (name.empty())
+         {
+            out << "--------------------------------------------------------------------------------" << endl;
+            continue;
+         }
+      
+         double  ncalls = (double)timers_[ii].total[NCALLS]; 
+         double  count = (double)timers_[ii].total[counter]; 
+         out << setw(maxLen) << left << name << " : "
+             << setw(10) << right << ncalls << "  |"
+             << setw(10) << timers_[ii].total[counter]*tick/ncalls 
+             << setw(10) << timers_[ii].total[counter]*tick 
+             << "   "
+             << setw(8) << setprecision(2) << 100.0*((timers_[ii].total[counter])/refCount)
+             << endl;
+      }
    }
    out.setf(oldFlags);
 }
@@ -401,86 +401,86 @@ void profileDumpStats(ostream& out)
        
       }
 
-   struct DoubleInt
-   {
-      double val;
-      int rank;
-   };
-   
-   DoubleInt minLoc[nTimers];
-   DoubleInt maxLoc[nTimers];
-   double sigma[nTimers];
-   {
-      DoubleInt minLocSendBuf[nTimers];
-      DoubleInt maxLocSendBuf[nTimers];
-      double sigmaSendBuf[nTimers];
-      
-      for (unsigned ii=0; ii<nTimers; ++ii)
+      struct DoubleInt
       {
-         minLocSendBuf[ii].rank = myRank;
-         maxLocSendBuf[ii].rank = myRank;
-         
-         HandleMap::const_iterator here = handleMap_.find(outputOrder[ii]);
-         if (here == handleMap_.end())
-         {
-            minLocSendBuf[ii].val = aveCount[ii];
-            maxLocSendBuf[ii].val = aveCount[ii];
-            sigmaSendBuf[ii] = 0;
-         }
-         else
-         {
-            //ewd: should these be set to zero for case when nActive[ii] == 0?
-            minLocSendBuf[ii].val = timers_[here->second].total[counter];
-            maxLocSendBuf[ii].val = timers_[here->second].total[counter];
-            sigmaSendBuf[ii] =     (timers_[here->second].total[counter] - aveCount[ii]);
-            if (nActive[ii] > 0)
-               sigmaSendBuf[ii] *= sigmaSendBuf[ii]/nActive[ii];
-            else
-               sigmaSendBuf[ii] = 0.0;
-               
-         }
-      }
-      MPI_Reduce(minLocSendBuf, minLoc, nTimers, MPI_DOUBLE_INT, MPI_MINLOC, 0, comm);
-      MPI_Reduce(maxLocSendBuf, maxLoc, nTimers, MPI_DOUBLE_INT, MPI_MAXLOC, 0, comm);
-      MPI_Reduce(sigmaSendBuf, sigma, nTimers, MPI_DOUBLE, MPI_SUM, 0, comm);
+         double val;
+         int rank;
+      };
    
-   if (myRank == 0) 
-   {
+      DoubleInt minLoc[nTimers];
+      DoubleInt maxLoc[nTimers];
+      double sigma[nTimers];
+      {
+         DoubleInt minLocSendBuf[nTimers];
+         DoubleInt maxLocSendBuf[nTimers];
+         double sigmaSendBuf[nTimers];
+      
+         for (unsigned ii=0; ii<nTimers; ++ii)
+         {
+            minLocSendBuf[ii].rank = myRank;
+            maxLocSendBuf[ii].rank = myRank;
+         
+            HandleMap::const_iterator here = handleMap_.find(outputOrder[ii]);
+            if (here == handleMap_.end())
+            {
+               minLocSendBuf[ii].val = aveCount[ii];
+               maxLocSendBuf[ii].val = aveCount[ii];
+               sigmaSendBuf[ii] = 0;
+            }
+            else
+            {
+               //ewd: should these be set to zero for case when nActive[ii] == 0?
+               minLocSendBuf[ii].val = timers_[here->second].total[counter];
+               maxLocSendBuf[ii].val = timers_[here->second].total[counter];
+               sigmaSendBuf[ii] =     (timers_[here->second].total[counter] - aveCount[ii]);
+               if (nActive[ii] > 0)
+                  sigmaSendBuf[ii] *= sigmaSendBuf[ii]/nActive[ii];
+               else
+                  sigmaSendBuf[ii] = 0.0;
+               
+            }
+         }
+         MPI_Reduce(minLocSendBuf, minLoc, nTimers, MPI_DOUBLE_INT, MPI_MINLOC, 0, comm);
+         MPI_Reduce(maxLocSendBuf, maxLoc, nTimers, MPI_DOUBLE_INT, MPI_MAXLOC, 0, comm);
+         MPI_Reduce(sigmaSendBuf, sigma, nTimers, MPI_DOUBLE, MPI_SUM, 0, comm);
+   
+         if (myRank == 0) 
+         {
    
 
-   string::size_type maxLen = 0;
-   for (unsigned ii=0; ii<nTimers; ++ii)
-      maxLen = max(maxLen, outputOrder[ii].size());
-   out << setw(10) << counterNames_[counter] 
-       << setw(maxLen-7) << " "
-       << setw(6) << "nTasks" << " |"
-       << setw(10) << "minTime" << " ( rank )"
-       << setw(10) << "maxTime" << " ( rank )"
-       << setw(10) << "sigma"
-       << setw(10) << "aveTime"
-       << endl;
-   out << "--------------------------------------------------------------------------------" << endl;
-   for (unsigned ii=0; ii<nTimers; ++ii)
-   {
-      if (aveCount[ii]*tick < 0.0001) continue;
-      if (outputOrder[ii].empty())
-      {
-         out << "--------------------------------------------------------------------------------" << endl;
-         continue;
+            string::size_type maxLen = 0;
+            for (unsigned ii=0; ii<nTimers; ++ii)
+               maxLen = max(maxLen, outputOrder[ii].size());
+            out << setw(10) << counterNames_[counter] 
+                << setw(maxLen-7) << " "
+                << setw(6) << "nTasks" << " |"
+                << setw(10) << "minTime" << " ( rank )"
+                << setw(10) << "maxTime" << " ( rank )"
+                << setw(10) << "sigma"
+                << setw(10) << "aveTime"
+                << endl;
+            out << "--------------------------------------------------------------------------------" << endl;
+            for (unsigned ii=0; ii<nTimers; ++ii)
+            {
+               if (aveCount[ii]*tick < 0.0001) continue;
+               if (outputOrder[ii].empty())
+               {
+                  out << "--------------------------------------------------------------------------------" << endl;
+                  continue;
+               }
+               out << setw(maxLen) << left << outputOrder[ii] << " : " << right
+                   << setw(6) << nActive[ii] << " |"
+                   << setprecision(3)
+                   << setw(10) << minLoc[ii].val*tick
+                   << " (" << setw(6) << minLoc[ii].rank << ")"
+                   << setw(10) << maxLoc[ii].val*tick
+                   << " (" << setw(6) << maxLoc[ii].rank << ")"
+                   << setw(10) << sqrt(sigma[ii])*tick
+                   << setw(10) << aveCount[ii]*tick
+                   << endl;
+            }
+         }
       }
-      out << setw(maxLen) << left << outputOrder[ii] << " : " << right
-          << setw(6) << nActive[ii] << " |"
-          << setprecision(3)
-          << setw(10) << minLoc[ii].val*tick
-          << " (" << setw(6) << minLoc[ii].rank << ")"
-          << setw(10) << maxLoc[ii].val*tick
-          << " (" << setw(6) << maxLoc[ii].rank << ")"
-          << setw(10) << sqrt(sigma[ii])*tick
-          << setw(10) << aveCount[ii]*tick
-          << endl;
-    }
-   }
-   }
    }
    if (myRank ==0 && nCounters_ > 2) 
    {
@@ -504,20 +504,20 @@ void profileDumpStats(ostream& out)
          double cycle=0.0; 
          for (unsigned jj=0; jj<nTimers; ++jj)
          {
-           int jjcoreID = ompID%nCore; 
-           int jjtimerID = ii%nMarks; 
-           if (jjcoreID == coreID && jjtimerID == timerID) cycle+=perfCount[jj][CYCLES]; 
+            int jjcoreID = ompID%nCore; 
+            int jjtimerID = ii%nMarks; 
+            if (jjcoreID == coreID && jjtimerID == timerID) cycle+=perfCount[jj][CYCLES]; 
          }
          double flop = 0.0 ; 
          if (cycle > 1e-9) flop = perfCount[ii][7]/(cycle*tick);
          
          if (perfCount[ii][NCALLS]>1e-9) 
          {
-         fprintf(file,"%4d %-32s %2d %2d %3d %12.0f",ii,(outputOrder[ii].c_str())+3,ompID,coreID,timerID,perfCount[ii][0]/nTasks); 
-         for (int jj=1;jj< nCounters_;jj++) fprintf(file," %7.3f", perfCount[ii][jj]*1e-9/nTasks); 
-         fprintf(file," %13.6f", flop*1e-9); 
-         fprintf(file,"\n"); 
-         fflush(file); 
+            fprintf(file,"%4d %-32s %2d %2d %3d %12.0f",ii,(outputOrder[ii].c_str())+3,ompID,coreID,timerID,perfCount[ii][0]/nTasks); 
+            for (int jj=1;jj< nCounters_;jj++) fprintf(file," %7.3f", perfCount[ii][jj]*1e-9/nTasks); 
+            fprintf(file," %13.6f", flop*1e-9); 
+            fprintf(file,"\n"); 
+            fflush(file); 
          }
       }
    }
