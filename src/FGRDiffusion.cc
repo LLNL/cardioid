f18ef4947f3dc5803152bdc55661fec0f99c7604
David Richards <richards12@llnl.gov>
2012-04-01 01:54:31 +0000

    FGRDiffusion.cc: Restore conditional that restricts updateVoltageBlock to                  a single thread.  It was erroneously removed in r533.                  The updateVoltageBlock function isn't yet set up for                  multiple threads.
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@534 baecf98e-1e81-4516-8920-d4c889e67ea4

--- src/FGRDiffusion.cc
+++ src/FGRDiffusion.cc
@@ -130,7 +130,7 @@ void FGRDiffusion::calc(const vector<double>& Vm, vector<double>& dVm, double *r
 #ifdef TIMING
    profileStart(FGR_Array2MatrixTimer);
 #endif
-   updateVoltageBlock(Vm, recv_buf_, nLocal);
+   if ( tid==0 ) updateVoltageBlock(Vm, recv_buf_, nLocal);
 #ifdef TIMING
    profileStop(FGR_Array2MatrixTimer);
    profileStart(FGR_BarrierTimer);
