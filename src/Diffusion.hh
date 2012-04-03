bd12d0424ab35800ec67ed9fc1f0e7d1ed8ff282
David Richards <richards12@llnl.gov>
2012-04-03 00:27:55 +0000

    Part 2 of getting rid of Saleheen.  Last check in banished it from the code, now it is also gone from the documentation, tests, and examples.
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@552 baecf98e-1e81-4516-8920-d4c889e67ea4

--- src/Diffusion.hh
+++ src/Diffusion.hh
@@ -11,13 +11,4 @@ class Diffusion
    calc(const std::vector<double>& Vm, std::vector<double>& dVm, double *recv_buf, int nLocal) = 0;
 };
 
-/** Notes:
- * 
- *  1. There are probably many things going on in the Saleheen98Diffusion
- *     concrete class that should be moved to the base class.  For
- *     example setting up and handling of the local bounding box
- *     and multiplying by diffusionscale seem generic.  This needs to
- *     be worked out when we build the next derived class.
- */
-
 #endif
