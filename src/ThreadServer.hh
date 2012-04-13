7e33d8bb080fb875450bfd31e588d2f903d1b4e0
David Richards <richards12@llnl.gov>
2012-04-13 22:57:26 +0000

    ThreadServer.hh: Cosmetic change
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@591 baecf98e-1e81-4516-8920-d4c889e67ea4

--- src/ThreadServer.hh
+++ src/ThreadServer.hh
@@ -36,8 +36,8 @@ class ThreadTeam
    int squadRank() const {return rankInfo_[omp_get_thread_num()].squadRank_;}
    int squadSize() const {return rankInfo_[omp_get_thread_num()].squadSize_;}
    const ThreadRankInfo& rankInfo()       const {return rankInfo_[omp_get_thread_num()];}
-   const ThreadRankInfo& rankInfo(int ii) const {return rankInfo_[ii];}
-   const ThreadHardwareInfo& hwInfo(int ii) const {return threads_[ii];}
+   const ThreadRankInfo& rankInfo(int ompID) const {return rankInfo_[ompID];}
+   const ThreadHardwareInfo& hwInfo(int ompID) const {return threads_[ompID];}
 
  private:
    void buildRankInfo();
