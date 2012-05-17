e622ca83b6c574615a8a38c2399ec11a09d8f804
David Richards <richards12@llnl.gov>
2012-05-17 18:10:52 +0000

    Removed the non-BGQ implementation of the nosync fast barriers and defined PER_SQUAD_BARRIER only on the BGQ build.
    This keeps in place the current squad level barriers on BGQ, but reverts
    to the usual non-optimzed barriers on other platforms.  More thought
    needs to go into the barrier implementations.  In particular, the
    current BGQ implementation is not fully correct.  It won't handle the
    case where different cores have different squad sizes.  This is not a
    common case, and it may never come up, but we should either handle it
    properly or make sure it doesn't happen.
    
    
    
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@734 baecf98e-1e81-4516-8920-d4c889e67ea4

--- src/fastBarrier_nosync.hh
+++ src/fastBarrier_nosync.hh
@@ -1,11 +1,9 @@
-#ifndef FASTBARRIER_NOSYNC__
-#define FASTBARRIER_NOSYNC__
+#ifndef FASTBARRIER_NOSYNC_HH
+#define FASTBARRIER_NOSYNC_HH
 
-#ifdef FAST_BARRIER_HH
 
+#ifdef BGQ
 #define PER_SQUAD_BARRIER
-
-#ifdef FAST_BARRIER_BGQ_HH
   /* This barrier code is correct for barriers executing
      entirely within one core.
   */
@@ -52,29 +50,6 @@ __INLINE__ void L2_Barrier_nosync_WaitAndReset(
   // prevent speculation
   __isync();
 }
+#endif  // #ifdef BGQ
+#endif  // #ifndef FASTBARRIER_NOSYNC_HH
 
-#else
-  /* No fast bgq barrier, use regular synchronizing fast barrier 
-     for the per squad barriers.
-  */
-
-__INLINE__ void L2_Barrier_nosync_Arrive(
-  L2_Barrier_t *b,        /* global barrier */
-  L2_BarrierHandle_t *h,  /* barrier handle private to this thread */
-  int eventNum)           /* number of arrival events */
-{
-  L2_BarrierWithSync_Arrive(b,h,eventNum);
-}
-__INLINE__ void L2_Barrier_nosync_WaitAndReset(
-  L2_Barrier_t *b,       /* global barrier */
-  L2_BarrierHandle_t *h, /* barrier handle private to this thread */
-  int eventNum)          /* number of arrival events */
-{
-  L2_BarrierWithSync_WaitAndReset(*b,h,eventNum);
-}
-
-
-#endif /* Fast bgq barrier if-statement */
-
-#endif /* Fast barrier if-statement */
-#endif /* Multiple inclusion protection */
