2a0c817ca03e7b67b9279e007c96558954d628cc
James Glosli <glosli1@llnl.gov>
2012-07-02 23:14:01 +0000

    TT06Func.cc:   Added epiRRG midRRG and endRRG cellTypes to the database
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@810 baecf98e-1e81-4516-8920-d4c889e67ea4

--- src/TT06Func.cc
+++ src/TT06Func.cc
@@ -462,6 +462,99 @@ map<string,CellTypeParmsFull>getStandardCellTypes()
       cellTypes[name].state["s_gate"]   = STATE(   GateVar, s_gate  , 0.999998);
       cellTypes[name].state["jL_gate"]  = STATE(   GateVar, jL_gate , 0.066   );
     }
+   {
+      int indices[] = {30,31,100} ;
+      name ="endoRRG"; 
+      cellTypes[name].name=name;
+      cellTypes[name].anatomyIndices.assign(indices,indices+3); 
+      cellTypes[name].s_switch=0; 
+      cellTypes[name].P_NaK=3.000;
+      cellTypes[name].g_Ks = 0.392; 
+      cellTypes[name].g_to = 0.073; 
+      cellTypes[name].g_NaL=0.15; 
+      cellTypes[name].Vm = -86.709;
+      cellTypes[name].state["K_i"]      = STATE(nonGateVar, K_i     , 138.4   );
+      cellTypes[name].state["Na_i"]     = STATE(nonGateVar, Na_i    , 10.355  );
+      cellTypes[name].state["Ca_i"]     = STATE(nonGateVar, Ca_i    , 0.00013 );
+      cellTypes[name].state["Ca_ss"]    = STATE(nonGateVar, Ca_ss   , 0.00036 );
+      cellTypes[name].state["Ca_SR"]    = STATE(nonGateVar, Ca_SR   , 3.715   );
+      cellTypes[name].state["R_prime"]  = STATE(nonGateVar, R_prime , 0.9068  );
+      cellTypes[name].state["fCass"]    = STATE(nonGateVar, fCass   , 0.9953  );
+      cellTypes[name].state["Xr1_gate"] = STATE(   GateVar, Xr1_gate, 0.00448 );
+      cellTypes[name].state["Xr2_gate"] = STATE(   GateVar, Xr2_gate, 0.476   );
+      cellTypes[name].state["Xs_gate"]  = STATE(   GateVar, Xs_gate , 0.0087  );
+      cellTypes[name].state["m_gate"]   = STATE(   GateVar, m_gate  , 0.00155 );
+      cellTypes[name].state["h_gate"]   = STATE(   GateVar, h_gate  , 0.7573  );
+      cellTypes[name].state["j_gate"]   = STATE(   GateVar, j_gate  , 0.7225  );
+      cellTypes[name].state["r_gate"]   = STATE(   GateVar, r_gate  , 2.235e-8);
+      cellTypes[name].state["d_gate"]   = STATE(   GateVar, d_gate  , 3.164e-5);
+      cellTypes[name].state["f_gate"]   = STATE(   GateVar, f_gate  , 0.8009  );
+      cellTypes[name].state["f2_gate"]  = STATE(   GateVar, f2_gate , 0.9778  );
+      cellTypes[name].state["s_gate"]   = STATE(   GateVar, s_gate  , 0.3212  );
+      cellTypes[name].state["jL_gate"]  = STATE(   GateVar, jL_gate , 0.066   );
+   }
+   {
+      int indices[] = { 76,101} ;
+      name="midRRG";
+      cellTypes[name].name=name;
+      cellTypes[name].anatomyIndices.assign(indices,indices+2);
+      cellTypes[name].s_switch=1; 
+      cellTypes[name].P_NaK=3.100;
+      cellTypes[name].g_Ks = 0.098; 
+      cellTypes[name].g_to = 0.294; 
+      cellTypes[name].g_NaL=0.3; 
+      cellTypes[name].Vm = -85.423;
+      cellTypes[name].state["K_i"]      = STATE(nonGateVar, K_i     , 138.52  );
+      cellTypes[name].state["Na_i"]     = STATE(nonGateVar, Na_i    , 10.132  );
+      cellTypes[name].state["Ca_i"]     = STATE(nonGateVar, Ca_i    , 0.000153);
+      cellTypes[name].state["Ca_ss"]    = STATE(nonGateVar, Ca_ss   , 0.00042 );
+      cellTypes[name].state["Ca_SR"]    = STATE(nonGateVar, Ca_SR   , 4.272   );
+      cellTypes[name].state["R_prime"]  = STATE(nonGateVar, R_prime , 0.8978  );
+      cellTypes[name].state["fCass"]    = STATE(nonGateVar, fCass   , 0.9942  );
+      cellTypes[name].state["Xr1_gate"] = STATE(   GateVar, Xr1_gate, 0.0165  );
+      cellTypes[name].state["Xr2_gate"] = STATE(   GateVar, Xr2_gate, 0.473   );
+      cellTypes[name].state["Xs_gate"]  = STATE(   GateVar, Xs_gate , 0.0174  );
+      cellTypes[name].state["m_gate"]   = STATE(   GateVar, m_gate  , 0.00165 );
+      cellTypes[name].state["h_gate"]   = STATE(   GateVar, h_gate  , 0.749   );
+      cellTypes[name].state["j_gate"]   = STATE(   GateVar, j_gate  , 0.6788  );
+      cellTypes[name].state["r_gate"]   = STATE(   GateVar, r_gate  , 2.347e-8);
+      cellTypes[name].state["d_gate"]   = STATE(   GateVar, d_gate  , 3.288e-5);
+      cellTypes[name].state["f_gate"]   = STATE(   GateVar, f_gate  , 0.7026  );
+      cellTypes[name].state["f2_gate"]  = STATE(   GateVar, f2_gate , 0.9526  );
+      cellTypes[name].state["s_gate"]   = STATE(   GateVar, s_gate  , 0.999998);
+      cellTypes[name].state["jL_gate"]  = STATE(   GateVar, jL_gate , 0.066   );
+   }
+   {
+      int indices[] = {77,102} ;
+      name="epiRRG";
+      cellTypes[name].name=name;
+      cellTypes[name].anatomyIndices.assign(indices,indices+2);
+      cellTypes[name].s_switch=1; 
+      cellTypes[name].P_NaK=3.000;
+      cellTypes[name].g_Ks = 0.392; 
+      cellTypes[name].g_to = 0.294; 
+      cellTypes[name].g_NaL=0.15; 
+      cellTypes[name].Vm  = -85.23;
+      cellTypes[name].state["K_i"]      = STATE(nonGateVar, K_i     , 136.89  );
+      cellTypes[name].state["Na_i"]     = STATE(nonGateVar, Na_i    , 8.604   );
+      cellTypes[name].state["Ca_i"]     = STATE(nonGateVar, Ca_i    , 0.000126);
+      cellTypes[name].state["Ca_ss"]    = STATE(nonGateVar, Ca_ss   , 0.00036 );
+      cellTypes[name].state["Ca_SR"]    = STATE(nonGateVar, Ca_SR   , 3.64    );
+      cellTypes[name].state["R_prime"]  = STATE(nonGateVar, R_prime , 0.9073  );
+      cellTypes[name].state["fCass"]    = STATE(nonGateVar, fCass   , 0.9953  );
+      cellTypes[name].state["Xr1_gate"] = STATE(   GateVar, Xr1_gate, 0.00621 );
+      cellTypes[name].state["Xr2_gate"] = STATE(   GateVar, Xr2_gate, 0.4712  );
+      cellTypes[name].state["Xs_gate"]  = STATE(   GateVar, Xs_gate , 0.0095  );
+      cellTypes[name].state["m_gate"]   = STATE(   GateVar, m_gate  , 0.00172 );
+      cellTypes[name].state["h_gate"]   = STATE(   GateVar, h_gate  , 0.7444  );
+      cellTypes[name].state["j_gate"]   = STATE(   GateVar, j_gate  , 0.7045  );
+      cellTypes[name].state["r_gate"]   = STATE(   GateVar, r_gate  , 2.42e-8 );
+      cellTypes[name].state["d_gate"]   = STATE(   GateVar, d_gate  , 3.373e-5);
+      cellTypes[name].state["f_gate"]   = STATE(   GateVar, f_gate  , 0.7888  );
+      cellTypes[name].state["f2_gate"]  = STATE(   GateVar, f2_gate , 0.9755  );
+      cellTypes[name].state["s_gate"]   = STATE(   GateVar, s_gate  , 0.999998);
+      cellTypes[name].state["jL_gate"]  = STATE(   GateVar, jL_gate , 0.066   );
+    }
     return cellTypes; 
 }
 }
