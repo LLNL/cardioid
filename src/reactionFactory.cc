1675d179b9e49580add46da97c9bf90338f5bb11
James Glosli <glosli1@llnl.gov>
2012-09-17 03:02:01 +0000

    reactionFactory.cc:   mod and jhTau stuff
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@1021 baecf98e-1e81-4516-8920-d4c889e67ea4

--- src/reactionFactory.cc
+++ src/reactionFactory.cc
@@ -131,8 +131,17 @@ namespace
       int fastNonGate =-1; 
       TT06Func::initCnst(); 
       string fitFit; 
+      if (object_testforkeyword(obj,"mod") )
+      {
+          //if (getRank(0) == 0) printf("keyword <mod> is deprecated by <jhTauSmooth>.  Replace keyword <mod> in the reaction object <%s>  with <jhTauSmooth>\n",obj->name); 
+          
+          //exit(1); 
+         objectGet(obj, "mod",          parms.jhTauSmooth, "0") ;
+      }
+      else
+         objectGet(obj, "jhTauSmooth",          parms.jhTauSmooth, "0") ;
       objectGet(obj, "tolerance",    parms.tolerance, "0.0") ;
-      objectGet(obj, "mod",          parms.mod, "0") ;
+      objectGet(obj, "jhTauSmooth",          parms.jhTauSmooth, "0") ;
       objectGet(obj, "fastReaction", parms.fastReaction, "-1") ;
       objectGet(obj, "fastGate",     fastGate, "-1") ;
       objectGet(obj, "fastNonGate",  fastNonGate, "-1") ;
