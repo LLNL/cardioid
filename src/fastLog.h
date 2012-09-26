205539f2d7d7f0300f668935ea75e5702cf4aac2
Tomas Oppelstrup <oppelstrup2@llnl.gov>
2012-09-26 18:45:33 +0000

    fastLog.h: New vectorized code for extracting mantissa and exponent   from floating point numbers. Gains about 2.5us on 16x16x14 blockPerformance   test case. There are defines/ifdefs to control use of new or old code. Currently   set to use a macro for exponent/mantissa extraction. The macro is set to fast   vectorized code on BGQ, and old style code otherwise.
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@1062 baecf98e-1e81-4516-8920-d4c889e67ea4

--- src/fastLog.h
+++ src/fastLog.h
@@ -36,17 +36,94 @@ static inline double fastLog(double x)
        return f ; 
 }
 
+
+/*
+  Defining LEGACY_EXPFIX makes the code identical to the one used before.
+  Not defining it, makes the fastLog functions use a macro to extract
+  mantissa and exponent from a vector of double precision numbers.
+
+  Defining VECTOR_PREFIX uses fast BGQ vectorized code.
+  Not defining it uses integer code identical to the legacy code
+  (except for being enclosed in a macro).
+ */
+//#define LEGACY_EXPFIX
+#ifdef BGQ
+#define VECTOR_EXPFIX
+#endif
+#ifdef VECTOR_EXPFIX
+/*
+  Fast vectorized floating point pipeline code for extracting
+  mantissa and exponent from vector of 4 double precision numbers.
+*/
+#define expfix(x,m,nn)							\
+  do {									\
+    const double scale = 1<<20, iscale = 1.0/scale;			\
+    const double iscale2 = iscale/(1<<12);				\
+    vector4double buf[1];						\
+    double *p = (double *) buf;						\
+    vector4double a,b,as,a2,b2,z1,z2;					\
+									\
+    vec_st(x,0,p);							\
+    z1 = vec_cfidu(vec_ldiz(0,(unsigned int *) p));			\
+    z2 = vec_cfidu(vec_ldiz(2*sizeof(double),(unsigned int *) p));	\
+									\
+    a = vec_perm(z1,z2, vec_gpci((0<<9) | (2<<6) | (4<<3) | 6) );	\
+    b = vec_perm(z1,z2, vec_gpci((1<<9) | (3<<6) | (5<<3) | 7) );	\
+									\
+    as = vec_trunc(vec_mul(a,vec_splats(iscale)));			\
+    nn = vec_sub(as,vec_splats(1023.0));				\
+									\
+    a2 = vec_nmsub(as,vec_splats(scale),a);				\
+    b2 = vec_madd(b,vec_splats(iscale2),a2);				\
+    m = vec_madd(b2,vec_splats(iscale),vec_splats(1.0));		\
+  } while(0)
+#else
+/*
+  Old serial integer pipeline code for extracting mantissa an
+  exponent from vector of 4 double precision numbers.
+*/
+#define expfix(x,m,nn)					\
+  do {							\
+    int n0,n1,n2,n3;					\
+    							\
+    union {unsigned int u[4][2]; vector4double d ;} t;	\
+    t.d=x;						\
+    							\
+    n0 = (t.u[0][LeadingBits]>>20)-1023;		\
+    t.u[0][LeadingBits] -= n0<<20;			\
+    							\
+    n1 = (t.u[1][LeadingBits]>>20)-1023;		\
+    t.u[1][LeadingBits] -= n1<<20;			\
+    							\
+    n2 = (t.u[2][LeadingBits]>>20)-1023;		\
+    t.u[2][LeadingBits] -= n2<<20;			\
+    							\
+    n3 = (t.u[3][LeadingBits]>>20)-1023;		\
+    t.u[3][LeadingBits] -= n3<<20;			\
+    							\
+    m = t.d;						\
+    {							\
+      vector4double nnx = {n0, n1, n2, n3};		\
+      nn = nnx;						\
+    }							\
+  } while(0)
+#endif
+
 static inline vector4double fastLog4(vector4double x4)
 {
   //vector4double y4 = {log(x4 get [0]), log(x4 get [1]), log(x4 get [2]), log(x4 get [3]) };
   //return y4; 
 
+#ifdef LEGACY_EXPFIX
   int n0,n1,n2,n3;
+#endif
   vector4double z, z2, m;
 
+#ifdef LEGACY_EXPFIX
   union {unsigned int u[4][2]; vector4double d ;} t;
   t.d=x4; 
-  
+#endif
+
   vector4double c0 = vec_splats(c[0]);
   vector4double c1 = vec_splats(c[1]);
   vector4double c2 = vec_splats(c[2]);
@@ -56,6 +133,7 @@ static inline vector4double fastLog4(vector4double x4)
   vector4double c6 = vec_splats(c[6]);
   vector4double c7 = vec_splats(c[7]);
 
+#ifdef LEGACY_EXPFIX
   n0 = (t.u[0][LeadingBits]>>20)-1023; 
   t.u[0][LeadingBits] -= n0<<20;
 
@@ -69,6 +147,10 @@ static inline vector4double fastLog4(vector4double x4)
   t.u[3][LeadingBits] -= n3<<20;
 
   m  = t.d;  
+#else
+  vector4double nn;
+  expfix(x4,m,nn);
+#endif
 
   vector4double m_sqrt1_2 = vec_splats(M_SQRT1_2);
   vector4double one = vec_splats(1.0);
@@ -80,7 +162,9 @@ static inline vector4double fastLog4(vector4double x4)
   z = vec_swdiv(zn, zd);
   z2 = vec_mul(z,z);
 
+#ifdef LEGACY_EXPFIX
   vector4double nn = {n0, n1, n2, n3};
+#endif
   nn = vec_mul(nn, vec_splats(M_LN2));
 
   vector4double f;
@@ -111,11 +195,16 @@ static inline vector4double fastLog8(vector4double x4, vector4double y4, vector4
   //*ret1 = fy; 
   //return fx; 
 
+#ifdef LEGACY_EXPFIX
   int n0,n1,n2,n3;
+#endif
+
   vector4double zx, z2x, nnx;
   vector4double zy, z2y, nny;
 
+#ifdef LEGACY_EXPFIX
   union {unsigned int u[4][2]; vector4double d ;} t;
+#endif
 
   vector4double c0 = vec_splats(c[0]);
   vector4double c1 = vec_splats(c[1]);
@@ -130,6 +219,7 @@ static inline vector4double fastLog8(vector4double x4, vector4double y4, vector4
   vector4double one = vec_splats(1.0);
 
   {
+#ifdef LEGACY_EXPFIX
     t.d=y4; 
   
     n0 = (t.u[0][LeadingBits]>>20)-1023; 
@@ -145,6 +235,10 @@ static inline vector4double fastLog8(vector4double x4, vector4double y4, vector4
     t.u[3][LeadingBits] -= n3<<20;
 
     vector4double m  = t.d;  
+#else
+    vector4double m,nn;
+    expfix(y4,m,nn);
+#endif
 
     vector4double zn = vec_msub(m_sqrt1_2,m,one);
     vector4double zd = vec_madd(m_sqrt1_2,m,one);
@@ -154,11 +248,15 @@ static inline vector4double fastLog8(vector4double x4, vector4double y4, vector4
     zx = vec_swdiv(zn, zd);
     z2x = vec_mul(zx,zx);
 
+
+#ifdef LEGACY_EXPFIX
     vector4double nn = {n0, n1, n2, n3};
+#endif
     nnx = vec_mul(nn, vec_splats(M_LN2));
   }
 
   {
+#ifdef LEGACY_EXPFIX
     t.d=x4; 
   
     n0 = (t.u[0][LeadingBits]>>20)-1023; 
@@ -174,6 +272,10 @@ static inline vector4double fastLog8(vector4double x4, vector4double y4, vector4
     t.u[3][LeadingBits] -= n3<<20;
 
     vector4double m  = t.d;  
+#else
+    vector4double m,nn;
+    expfix(x4,m,nn);
+#endif
 
     vector4double zn = vec_msub(m_sqrt1_2,m,one);
     vector4double zd = vec_madd(m_sqrt1_2,m,one);
@@ -183,7 +285,9 @@ static inline vector4double fastLog8(vector4double x4, vector4double y4, vector4
     zy = vec_swdiv(zn, zd);
     z2y = vec_mul(zy,zy);
 
+#ifdef LEGACY_EXPFIX
     vector4double nn = {n0, n1, n2, n3};
+#endif
     nny = vec_mul(nn, vec_splats(M_LN2));
 
   }
