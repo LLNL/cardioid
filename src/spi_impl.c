f5e73227d8af7ee33fcc679c41938976ebd75aa1
Changhuan Kim <kimchang@us.ibm.com>
2012-10-12 18:32:37 +0000

    when time-out, it prints out physical coordinatE
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@1112 baecf98e-1e81-4516-8920-d4c889e67ea4

--- src/spi_impl.c
+++ src/spi_impl.c
@@ -616,8 +616,15 @@ void complete_spi_alter_monitor(spi_hdl_t* spi_hdl, uint32_t recv_size, int32_t*
     {
       volatile int recved=recv_cnt[ii+bw*MAX_RECV_NUM]+(recv_offset[ii+1]-recv_offset[ii])*width;
 //      if(knt%10==0) printf("%d ",recved);
-      if(recved != 0) printf("node:%d did NOT receive %d bytes from node:%d while expecting %d\n",myID,recved,recv_task[ii],(recv_offset[ii+1]-recv_offset[ii])*width); 
-      else printf("node:%d did receive all the packets from node:%d\n",myID,recv_task[ii]);
+      if(recved != 0)
+      {
+         printf("node:%d did NOT receive %d bytes from node:%d while expecting %d\n",myID,recved,recv_task[ii],(recv_offset[ii+1]-recv_offset[ii])*width); 
+         
+         MUHWI_Destination_t* mapping = spi_hdl->mapping_hdl;
+         printf("node:%d=(%d,%d,%d,%d,%d)\n",myID,(mapping)[myID].Destination.A_Destination,(mapping)[myID].Destination.B_Destination,(mapping)[myID].Destination.C_Destination,(mapping)[myID].Destination.D_Destination,(mapping)[myID].Destination.E_Destination);
+         printf("node:%d=(%d,%d,%d,%d,%d)\n",recv_task[ii],(mapping)[recv_task[ii]].Destination.A_Destination,(mapping)[recv_task[ii]].Destination.B_Destination,(mapping)[recv_task[ii]].Destination.C_Destination,(mapping)[recv_task[ii]].Destination.D_Destination,(mapping)[recv_task[ii]].Destination.E_Destination);
+     } 
+     else printf("node:%d did receive all the packets from node:%d\n",myID,recv_task[ii]);
     }
     //wait more for others to quit
     for(knt=0;knt<MAX_WAIT ;knt++) { }
