bd37a0dfd84b68abfef5093fd5738955ecee7162
Tomas Oppelstrup <oppelstrup2@llnl.gov>
2012-10-24 18:22:38 +0000

    Added function integrateLoop_nostim(), with the same calling signature as integrateLoop(). integrateLoop_nostim() disreagrds the "stim" argument, which seems to save 0.3us in the integrator, and 1us on the total simulation loop for a 16x16x14 single node blockperformance test case.
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@1179 baecf98e-1e81-4516-8920-d4c889e67ea4

--- src/clooper.c
+++ src/clooper.c
@@ -45,8 +45,10 @@
 
 #ifdef BGQ
 #define integrateLoop_vector integrateLoop
+#define integrateLoop_vector_nostim integrateLoop_nostim
 #else
 #define integrateLoop_normal integrateLoop
+#define integrateLoop_normal_nostim integrateLoop_nostim
 #endif
 /* Other options for the "integrateLoop" macro...
 //#define integrateLoop_normal integrateLoop
@@ -68,6 +70,26 @@ void integrateLoop_normal(const int begin, const int end,
       int ib = blockOffset[ii];
 
       double dVm = dVmR[ii] + stim[ii] + diffusionScale * dVmBlock[ib];
+      Vm[ii] += dt*dVm;
+      VmBlock[ib] = Vm[ii];
+   }
+   return;
+}
+void integrateLoop_normal_nostim(const int begin, const int end,
+                   const double dt,
+                   double* dVmR,
+                   double* stim,
+                   unsigned* blockOffset,
+                   double* dVmBlock,
+                   double* VmBlock,
+                   double* Vm,
+                   double diffusionScale)
+{
+   for (unsigned ii=begin; ii<end; ++ii)
+   {
+      int ib = blockOffset[ii];
+
+      double dVm = dVmR[ii] + /*stim[ii] +*/ diffusionScale * dVmBlock[ib];
       Vm[ii] += dt*dVm;
       VmBlock[ib] = Vm[ii];
    }
@@ -117,7 +139,7 @@ void integrateLoop_vector(const int begin, const int end,
   for(ii = 0; ii<n; ii++) {
     if( (((unsigned long long int) Vm)/sizeof(double)) % 4 == 3) break;
     int ib = *(++blockOffset);
-    
+
     double dVm = *(++dVmR) + *(++stim) + diffusionScale * dVmBlock[ib];
     double dv = dt*dVm + *(++Vm);
     *Vm = dv;
@@ -184,8 +206,104 @@ void integrateLoop_vector(const int begin, const int end,
       Vm += ii;
       for(; ii<n; ii++) {
 	int ib = *(++blockOffset);
-	
 	double dVm = *(++dVmR) + *(++stim) + diffusionScale * dVmBlock[ib];
+	double dv = dt*dVm + *(++Vm);
+	*Vm = dv;
+	VmBlock[ib] = dv;
+      }
+    }
+
+#undef shiftload
+#undef makeshift
+
+  }
+
+}
+
+void integrateLoop_vector_nostim(const int begin, const int end,
+                   const double dt,
+                   double* dVmR,
+                   double* stim,
+                   unsigned* blockOffset,
+                   double* dVmBlock,
+                   double* VmBlock,
+                   double* Vm,
+                   double diffusionScale)
+{
+  unsigned int n = end - begin;
+  unsigned ii;
+
+  dVmR += begin - 1;
+  blockOffset += begin-1;
+  Vm += begin - 1;
+  
+  for(ii = 0; ii<n; ii++) {
+    if( (((unsigned long long int) Vm)/sizeof(double)) % 4 == 3) break;
+    int ib = *(++blockOffset);
+    double dVm = *(++dVmR) + diffusionScale * dVmBlock[ib];
+    double dv = dt*dVm + *(++Vm);
+    *Vm = dv;
+    VmBlock[ib] = dv;
+  }
+
+  n = n - ii;
+  if(n > 0) {
+    vector4double diffScale_val = {diffusionScale,diffusionScale,diffusionScale,diffusionScale};
+    vector4double dt_val = {dt,dt,dt,dt};
+
+#define makeshift(name) \
+    vector4double name##_shift = vec_lvsl(0,name-3); \
+    vector4double *name##_ptr = (vector4double *) (name-3); \
+    vector4double name##_val,name##_v1,name##_v2 = *(++name##_ptr)
+#define shiftload(name) \
+    name##_v1 = name##_v2; \
+    name##_v2 = *(++name##_ptr); \
+    name##_val = vec_perm(name##_v1,name##_v2,name##_shift)
+
+    const int n4 = n>>2;
+    makeshift(dVmR);
+    vector4double *Vm_ptr = (vector4double *) (Vm-3);
+    for(ii = 0; ii<n4; ii++) {
+      vector4double Vm_val = *(++Vm_ptr),x;
+      shiftload(dVmR);
+
+      int ib1 = *(++blockOffset) * sizeof(double);
+      int ib2 = *(++blockOffset) * sizeof(double);
+      int ib3 = *(++blockOffset) * sizeof(double);
+      int ib4 = *(++blockOffset) * sizeof(double);
+
+      {
+	vector4double y1,y2,y3;
+	//x[0] = dVmBlock[ib];
+	asm("lfdx %0,%1,%2" : "=v"(x) : "b"(ib1) , "b"(dVmBlock));
+	asm("lfdx %0,%1,%2" : "=v"(y1) : "b"(ib2) , "b"(dVmBlock));
+	x = vec_sldw(x,y1,1);
+	asm("lfdx %0,%1,%2" : "=v"(y2) : "b"(ib3) , "b"(dVmBlock));
+	x = vec_sldw(x,y2,1);
+	asm("lfdx %0,%1,%2" : "=v"(y3) : "b"(ib4) , "b"(dVmBlock));
+	x = vec_sldw(x,y3,1);
+      }
+
+      vector4double dVm = vec_madd(diffScale_val , x, dVmR_val) ;
+      vector4double dv = vec_madd(  dt_val,dVm , Vm_val);
+      *Vm_ptr = dv;
+      asm("stfdx %0,%1,%2" : : "v"(dv ) , "b"(ib1) , "b"(VmBlock));
+      vector4double dv1 = vec_sldw(dv,dv,1);
+      asm("stfdx %0,%1,%2" : : "v"(dv1) , "b"(ib2) , "b"(VmBlock));
+      vector4double dv2 = vec_sldw(dv,dv,2);
+      asm("stfdx %0,%1,%2" : : "v"(dv2) , "b"(ib3) , "b"(VmBlock));
+      vector4double dv3 = vec_sldw(dv,dv,3);
+      asm("stfdx %0,%1,%2" : : "v"(dv3) , "b"(ib4) , "b"(VmBlock));
+    }
+
+    ii = ii << 2;
+    if(ii < n) {
+      dVmR += ii;
+      stim += ii;
+      Vm += ii;
+      for(; ii<n; ii++) {
+	int ib = *(++blockOffset);
+	double dVm = *(++dVmR) + diffusionScale * dVmBlock[ib];
 	double dv = dt*dVm + *(++Vm);
 	*Vm = dv;
 	VmBlock[ib] = dv;
