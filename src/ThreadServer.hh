b0c864a1a885f569ab0f26bbdc7fffbdaf178b4a
David Richards <richards12@llnl.gov>
2013-03-27 21:29:03 +0000

    ThreadServer.hh:  Cosmetic changes.
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@1417 baecf98e-1e81-4516-8920-d4c889e67ea4

--- src/ThreadServer.hh
+++ src/ThreadServer.hh
@@ -33,18 +33,18 @@ class ThreadTeam
    int nThreads()  const {return threads_.size();}
    int nSquads()   const {return nSquads_;}
    int teamRank()  const {return rankInfo_[omp_get_thread_num()].teamRank_;}
-   int teamRank(int ompId )  const {return rankInfo_[ompId].teamRank_;}
+   int teamRank(int ompId)  const {return rankInfo_[ompId].teamRank_;}
    int squadRank() const {return rankInfo_[omp_get_thread_num()].squadRank_;}
    int squadSize() const {return rankInfo_[omp_get_thread_num()].squadSize_;}
-   const ThreadRankInfo& rankInfo()       const {return rankInfo_[omp_get_thread_num()];}
+   const ThreadRankInfo& rankInfo()          const {return rankInfo_[omp_get_thread_num()];}
    const ThreadRankInfo& rankInfo(int ompID) const {return rankInfo_[ompID];}
-   const ThreadHardwareInfo& hwInfo(int ompID) const {return threads_[ompID];}
+   const ThreadHardwareInfo& hwInfo(int ii) const {return threads_[ii];} // not ranked by ompId or teamRank
 
  private:
    void buildRankInfo();
    int nSquads_;
-   std::vector<ThreadHardwareInfo> threads_;
-   std::vector<ThreadRankInfo> rankInfo_;
+   std::vector<ThreadHardwareInfo> threads_; // info only on threads in team
+   std::vector<ThreadRankInfo> rankInfo_;    // info for all omp ids.
 };
 
 std::ostream& operator<<(std::ostream& out, const ThreadTeam& tt);
