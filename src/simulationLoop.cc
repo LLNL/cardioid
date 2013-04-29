LLNL/cardioid

commit 7a7047dbf0a9071174aa482f0a00597de20ea4be

Refactor I/O related code in the simulation loop.  Printing of the data file is now handled outside of loopIO.  I *think* all of the sensors are still working, but I confess I haven't checked.
git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@1111 baecf98e-1e81-4516-8920-d4c889e67ea4

src/simulationLoop.cc
@@ -54,6 +54,33 @@ namespace
    }
 }
 
+namespace
+{
+   void printData(const Simulate& sim)
+   {
+      startTimer(printDataTimer);
+      int pi = sim.printIndex_; 
+      if (pi >= 0)
+      {
+         const VectorDouble32& VmArray(sim.vdata_.VmArray_);
+         const VectorDouble32& dVmReaction(sim.vdata_.dVmReaction_);
+         const VectorDouble32& dVmDiffusion(sim.vdata_.dVmDiffusion_);
+         const unsigned* const blockIndex = sim.diffusion_->blockIndex();
+         const double* const dVdMatrix = sim.diffusion_->dVmBlock();
+         const double scale = sim.diffusion_->diffusionScale();
+         const int index = blockIndex[pi];
+         double dVd = dVmDiffusion[pi] + scale*dVdMatrix[index];
+         printf("%8d %8.3f %12lld %21.15f %21.15f %21.15f\n",
+                sim.loop_, sim.time_, sim.anatomy_.gid(pi),
+                VmArray[pi], dVmReaction[pi], dVd);  
+         fprintf(sim.printFile_,
+                 "%8d %8.3f %12lld %21.15f %21.15f %21.15f\n",
+                 sim.loop_, sim.time_, sim.anatomy_.gid(pi),
+                 VmArray[pi], dVmReaction[pi], dVd);  
+      }
+      stopTimer(printDataTimer);
+   }
+}
 
 void simulationProlog(Simulate& sim)
 {
@@ -95,18 +122,6 @@ void loopIO(const Simulate& sim,int firstCall)
    stopTimer(sensorTimer);
       
    startTimer(loopIOTimer);
-   {
-   if ( loop % sim.printRate_ == 0) 
-   {
-      int printIndex = sim.printIndex_; 
-      if (printIndex >= 0)
-      {
-         printf("%8d %8.3f %12lld %21.15f %21.15f %21.15f\n",loop,sim.time_,sim.anatomy_.gid(printIndex),VmArray[printIndex],dVmR[printIndex],dVmD[printIndex]);  
-         fprintf(sim.printFile_,"%8d %8.3f %12lld %21.15f %21.15f %21.15f\n",loop,sim.time_,sim.anatomy_.gid(printIndex),VmArray[printIndex],dVmR[printIndex],dVmD[printIndex]); 
-      }
-   }
-   }
-
    if (sim.loop_ > 0 && sim.checkpointRate_ > 0 && sim.loop_ % sim.checkpointRate_ == 0)
       writeCheckpoint(sim, MPI_COMM_WORLD);
 
@@ -146,6 +161,7 @@ void simulationLoop(Simulate& sim)
    cout << "Rank[" << myRank << "]: numOfNeighborToSend=" << sim.commTable_->_sendTask.size() << " numOfNeighborToRecv=" << sim.commTable_->_recvTask.size() << " numOfBytesToSend=" << sim.commTable_->_sendOffset[sim.commTable_->_sendTask.size()]*sizeof(double) << " numOfBytesToRecv=" << sim.commTable_->_recvOffset[sim.commTable_->_recvTask.size()]*sizeof(double) << endl;
 #endif
 
+   printData(sim);
    loopIO(sim,1);
    profileStart(simulationLoopTimer);
    while ( sim.loop_<sim.maxLoop_ )
@@ -201,6 +217,7 @@ void simulationLoop(Simulate& sim)
       
       if( sim.checkIO() )sim.bufferReactionData();
       
+      printData(sim);
       loopIO(sim,0);
    }
    profileStop(simulationLoopTimer);
@@ -510,7 +527,8 @@ void reactionLoop(Simulate& sim, SimLoopData& loopData, L2_BarrierHandle_t& reac
 
    if (tid == 0) 
    {
-     loopIO(sim,1);
+      printData(sim);
+      loopIO(sim,1);
    }
    uint64_t loopLocal = sim.loop_;
    while ( loopLocal < sim.maxLoop_ )
@@ -595,6 +613,9 @@ void reactionLoop(Simulate& sim, SimLoopData& loopData, L2_BarrierHandle_t& reac
          ++sim.loop_;
       }
       
+      if ( tid == 0 && sim.loop_ % sim.printRate_ == 0) 
+         printData(sim);
+
       if ( sim.checkIO(loopLocal) ) // every thread should return the same result
       {
          // jlf: needed to have correct dVm in sensors.
@@ -612,12 +633,15 @@ void reactionLoop(Simulate& sim, SimLoopData& loopData, L2_BarrierHandle_t& reac
          }
             
          sim.bufferReactionData(begin, end);
+         L2_BarrierWithSync_Barrier(loopData.reactionWaitOnNonGateBarrier,
+                                    &reactionWaitOnNonGateHandle,
+                                    sim.reactionThreads_.nThreads());
+         if (tid == 0) loopIO(sim,0);
       }
          
       L2_BarrierWithSync_Barrier(loopData.reactionWaitOnNonGateBarrier,
                                  &reactionWaitOnNonGateHandle,
                                  sim.reactionThreads_.nThreads());
-      if (tid == 0) loopIO(sim,0);
       stopTimer(reactionMiscTimer); 
          
       startTimer(haloMove2BufTimer);
@@ -742,6 +766,7 @@ void simulationLoopAllSkate(Simulate& sim)
    simulationProlog(sim);
    HaloExchange<double, AlignedAllocator<double> >
       voltageExchange(sim.sendMap_, (sim.commTable_));
+   printData(sim);
    loopIO(sim, 1);
 
    // define useful aliases
@@ -863,26 +888,7 @@ void simulationLoopAllSkate(Simulate& sim)
          }
          
          if ( tid == 0 && sim.loop_ % sim.printRate_ == 0) 
-         {
-            startTimer(printDataTimer);
-            int pi = sim.printIndex_; 
-            if (pi >= 0)
-            {
-               const unsigned* const blockIndex = sim.diffusion_->blockIndex();
-               const double* const dVdMatrix = sim.diffusion_->dVmBlock();
-               const double scale = sim.diffusion_->diffusionScale();
-               const int index = blockIndex[pi];
-               double dVd = dVmDiffusion[pi] + scale*dVdMatrix[index];
-               printf("%8d %8.3f %12lld %21.15f %21.15f %21.15f\n",
-                      sim.loop_, sim.time_, sim.anatomy_.gid(pi),
-                      VmArray[pi], dVmReaction[pi], dVd);  
-               fprintf(sim.printFile_,
-                       "%8d %8.3f %12lld %21.15f %21.15f %21.15f\n",
-                       sim.loop_, sim.time_, sim.anatomy_.gid(pi),
-                       VmArray[pi], dVmReaction[pi], dVd);  
-            }
-            stopTimer(printDataTimer);
-         }
+            printData(sim);
          
 //         sensors;
 //         checkpoint;

src/Simulate.cc
@@ -70,18 +70,16 @@ void Simulate::outOfRange(unsigned index, double dVmr, double dVmd)
 // check if any IO may be needed at this step
 bool Simulate::checkIO(int loop) const
 {
-   if( loop<0 )loop=loop_;
+   if ( loop<0 )loop=loop_;
    
-   if( loop % printRate_ == 0 )return true;
-   if( loop > 0 && checkpointRate_ > 0 
+   if ( loop > 0 && checkpointRate_ > 0 
                 && loop % checkpointRate_ == 0)return true;
    if (loop > 0 && loop % snapshotRate_ == 0)return true;
    
-   for(std::vector<Sensor*>::const_iterator is = sensor_.begin(); 
-                                            is!= sensor_.end();
-                                          ++is)
+   for (unsigned ii=0; ii<sensor_.size(); ++ii)
    {
-      if( (*is)->checkPrintAtStep(loop) )return true;
+      if ( sensor_[ii]->checkPrintAtStep(loop) )
+         return true;
    }
    
    return false;

commit 7e0d8587da24766d7bbe495eb52f5f7611a5c808

simulationLoop.cc: Fix bug introduced in r1111.  In that revision I    refactored the loopIO function to improve performance, however, I    didn't account for the fact that not all Diffusion classes have    internal storage for the diffusion derivative.  This fixes that    problem.  The root cause is the fact that we have different Diffusion    concrete classes that behave very differently with respect to    where they place the derivatives they calculate.  Until we fix that    problem this sort of thing is likely to continue.
git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@1495 baecf98e-1e81-4516-8920-d4c889e67ea4

src/simulationLoop.cc
@@ -71,10 +71,14 @@ namespace
          const VectorDouble32& dVmReaction(sim.vdata_.dVmReaction_);
          const VectorDouble32& dVmDiffusion(sim.vdata_.dVmDiffusion_);
          const unsigned* const blockIndex = sim.diffusion_->blockIndex();
-         const double* const dVdMatrix = sim.diffusion_->dVmBlock();
-         const double scale = sim.diffusion_->diffusionScale();
-         const int index = blockIndex[pi];
-         double dVd = dVmDiffusion[pi] + scale*dVdMatrix[index];
+         double dVd = dVmDiffusion[pi];
+         if (blockIndex)
+         {
+            const double* const dVdMatrix = sim.diffusion_->dVmBlock();
+            const double scale = sim.diffusion_->diffusionScale();
+            const int index = blockIndex[pi];
+            dVd += scale*dVdMatrix[index];
+         }
          printf("%8d %8.3f %12lld %21.15f %21.15f %21.15f\n",
                 sim.loop_, sim.time_, sim.anatomy_.gid(pi),
                 VmArray[pi], dVmReaction[pi], dVd);  