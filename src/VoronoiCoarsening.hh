c761f6796ce9ad01977adfbba2a0f1b47ea39038
David Richards <richards12@llnl.gov>
2013-05-16 16:52:04 +0000

    VoronoiCoarsening.hh: Purely cosmetic
    git-svn-id: svn+ssh://lc-svn.llnl.gov/usr/gapps/emhm/svn/Cardioid/EP/trunk@1534 baecf98e-1e81-4516-8920-d4c889e67ea4

--- src/VoronoiCoarsening.hh
+++ src/VoronoiCoarsening.hh
@@ -15,6 +15,78 @@
 #include <cassert>
 #include <iostream>
 class CommTable;
+class LocalSums;
+
+class VoronoiCoarsening
+{
+ public:
+   VoronoiCoarsening(const Anatomy& anatomy,
+                     const std::vector<Long64>& gid,
+                     const CommTable* commtable);
+   void computeRemoteTasks();
+   void exchangeAndSum(LocalSums& valcolors);
+   void exchangeAndSum(std::vector<LocalSums*> valcolors);
+   int bruteForceColoring(const double max_distance=100000.);
+   void colorDisplacements(std::vector<double>& dx,
+                           std::vector<double>& dy,
+                           std::vector<double>& dz);
+   void accumulateValues(const VectorDouble32& val, LocalSums& valcolors);
+   
+   const Vector center(const int color)const
+   {
+      std::map<int, Long64>::const_iterator here = centers_.find(color);
+      assert( here!=centers_.end() );
+      return indexToVector_(here->second);
+   }
+   short multiplicity(const int color)const
+   {
+      return (*multiplicities_.find(color)).second;
+   }
+   const std::set<int>& getOwnedColors()const {return owned_colors_;}
+   const std::set<int>& getLocalColors()const {return local_colors_;}
+   int getColor(const int ic)const {return cell_colors_[ic]; }
+
+ private:
+   std::map<int,int> ncolors_to_send_; // remote pe -> number of colors
+   std::map<int,int> ncolors_to_recv_; // remote pe -> number of colors
+
+   std::set<int> remote_tasks_;
+   std::set<int> dst_tasks_;
+   std::set<int> src_tasks_;
+
+   std::set<int> local_colors_;
+
+   void computeColorAverages(const std::vector<double>& val);
+   void computeColorCenterValues(const std::vector<double>& val);
+
+   std::map<int,int> ncolors_; // color -> number of local cells of that color
+   
+   MPI_Comm comm_;
+   const CommTable* commTable_;
+
+   const Anatomy& anatomy_;
+
+   // shared globally
+   std::map<int, Long64> centers_; // color->cell gid
+   std::map<int,short>  multiplicities_; // color->multiplicity
+
+   // local only
+   std::vector<int> cell_colors_; // colors of local cells
+
+   // set of colors local task is responsible for
+   std::set<int> owned_colors_;
+   
+   IndexToVector indexToVector_;
+
+   void setupComm(const std::map< int, int* >& nremote_colors_for_task, 
+                const int size_nremote_colors_for_task);
+
+   Vector getDomaincenter()const;
+   double getDomainRadius(const Vector& domain_center)const;
+   std::map<int,Vector> getCloseCenters(const Vector& domain_center, 
+                                        const double)const;
+   
+};
 
 // packed format for communications
 struct PackedData
@@ -140,88 +212,5 @@ class LocalSums
    }
 };
 
-
-class VoronoiCoarsening
-{
- private:
-   std::map<int,int> ncolors_to_send_; // remote pe -> number of colors
-   std::map<int,int> ncolors_to_recv_; // remote pe -> number of colors
-
-   std::set<int> remote_tasks_;
-   std::set<int> dst_tasks_;
-   std::set<int> src_tasks_;
-
-   std::set<int> local_colors_;
-
-   void computeColorAverages(const std::vector<double>& val);
-   void computeColorCenterValues(const std::vector<double>& val);
-
-   std::map<int,int> ncolors_; // color -> number of local cells of that color
-   
-   MPI_Comm comm_;
-   const CommTable* commTable_;
-
-   const Anatomy& anatomy_;
-
-   // shared globally
-   std::map<int, Long64> centers_; // color->cell gid
-   std::map<int,short>  multiplicities_; // color->multiplicity
-
-   // local only
-   std::vector<int> cell_colors_; // colors of local cells
-
-   // set of colors local task is responsible for
-   std::set<int> owned_colors_;
-   
-   IndexToVector indexToVector_;
-
-   void setupComm(const std::map< int, int* >& nremote_colors_for_task, 
-                const int size_nremote_colors_for_task);
-
-   Vector getDomaincenter()const;
-   double getDomainRadius(const Vector& domain_center)const;
-   std::map<int,Vector> getCloseCenters(const Vector& domain_center, 
-                                        const double)const;
-   
- public:
-   VoronoiCoarsening(const Anatomy& anatomy,
-                     const std::vector<Long64>& gid,
-                     const CommTable* commtable);
-   void computeRemoteTasks();
-   void exchangeAndSum(LocalSums& valcolors);
-   void exchangeAndSum(std::vector<LocalSums*> valcolors);
-   int bruteForceColoring(const double max_distance=100000.);
-   void colorDisplacements(std::vector<double>& dx,
-                           std::vector<double>& dy,
-                           std::vector<double>& dz);
-   void accumulateValues(const VectorDouble32& val, LocalSums& valcolors);
-   
-   const Vector center(const int color)const
-   {
-      std::map<int, Long64>::const_iterator here = centers_.find(color);
-      assert( here!=centers_.end() );
-      return indexToVector_(here->second);
-   }
-   
-   short multiplicity(const int color)const
-   {
-      return (*multiplicities_.find(color)).second;
-   }
-   
-   const std::set<int>& getOwnedColors()const
-   {
-      return owned_colors_;
-   }
-   
-   const std::set<int>& getLocalColors()const
-   {
-      return local_colors_;
-   }
-   
-   int getColor(const int ic)const
-   {
-      return cell_colors_[ic];
-   }
-};
 
 #endif
