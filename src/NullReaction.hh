878b0000f1fa51726a02300ac05df11dfcae6a5b
Robert Blake <blake14@llnl.gov>
2017-11-29 10:52:19 -0800

    Null reaction can now be used in the rest of the code.

--- src/NullReaction.hh
+++ src/NullReaction.hh
@@ -27,7 +27,15 @@ class NullReaction : public Reaction
       for (unsigned ii=0; ii<Vm.size(); ++ii)
          Vm[ii] = V0_;
    };
-
+   void getCheckpointInfo(std::vector<std::string>& fieldNames,
+                          std::vector<std::string>& fieldUnits) const {
+      fieldNames.clear();
+      fieldUnits.clear();
+   }
+   virtual int getVarHandle(const std::string& varName) const {
+      return -1;
+   }
+   
  private:
    double V0_;
 
