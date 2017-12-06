c4d3d0b51e28607e1c5887fd52462a03dbdb9d0f
Changhoan Kim <ckim@mbp.watson.ibm.com>
2017-12-06 13:59:10 -0500

    some test in

--- test/CUDADiffusion/CUDADiffusion_ker.cu
+++ test/CUDADiffusion/CUDADiffusion_ker.cu
@@ -194,9 +194,12 @@ int main()
   int nz=52;
 
   Real *VmRaw,*dVmRaw,*sigmaRaw,*dVmOut;
+  Real *h_sigma, *h_dVm;
   cudaMalloc(&VmRaw,sizeof(double)*nx*ny*nz);
   cudaMalloc(&dVmRaw,sizeof(double)*nx*ny*nz);
   cudaMalloc(&sigmaRaw,sizeof(double)*nx*ny*nz*9);
+  h_sigma=(Real*)malloc(sizeof(double)*nx*ny*nz*9);
+  h_dVm=(Real*)malloc(sizeof(double)*nx*ny*nz);
 
   cudaMemset(VmRaw,1,sizeof(double)*nx*ny*nz);
   cudaMemset(dVmRaw,1,sizeof(double)*nx*ny*nz);
@@ -205,7 +208,46 @@ int main()
   printf("running with nx=%d ny=%d nz=%d\n",nx,ny,nz);
   call_cuda_kernels(VmRaw,dVmRaw,sigmaRaw,nx,ny,nz,0,0,nx*ny*nz);
 
+  cudaMemset(VmRaw,0,sizeof(double)*nx*ny*nz);
+  cudaMemset(dVmRaw,0,sizeof(double)*nx*ny*nz);
+  cudaMemset(sigmaRaw,0,sizeof(double)*nx*ny*nz*9);
+
+  //test 1
+  //set 1 at ii,jj,kk
+  //set sigmaXYZ 1
+  Real value=1.;
+  int ii=jj=kk=3;
+  cudaMemcpy( &(VmRaw[ kk + nz*( jj + ny* ii ) ]) , &value, sizeof(double), , cudaMemcpyHostToDevice );
+
+  #define sigmaX(x,y,z,dir) h_sigma[ z + Lkk * ( y + Ljj * ( x + Lii * dir ) ) ]
+  #define sigmaY(x,y,z,dir) h_sigma[ z + Lkk * ( y + Ljj * ( x + Lii * dir ) ) + 3*nx*ny*nz ]
+  #define sigmaZ(x,y,z,dir) h_sigma[ z + Lkk * ( y + Ljj * ( x + Lii * dir ) ) + 6*nx*ny*nz ]
+
+  for(int ii=0;ii<nx;ii++)
+  for(int jj=0;jj<ny;jj++)
+  for(int kk=0;kk<nz;kk++)
+  {
+      sigmaX(ii,jj,kk,0) = 1;
+      sigmaY(ii,jj,kk,1) = 1;
+      sigmaZ(ii,jj,kk,2) = 1;
+  }
+      
+  cudaMemcpy( sigmaRaw, h_sigma, sizeof(double) * 9 * nx*ny*nz , cudaMemcpyHostToDevice );
+
+  call_cuda_kernels(VmRaw,dVmRaw,sigmaRaw,nx,ny,nz,0,0,nx*ny*nz);
+
+  cudaMemcpy( h_dVm, dVmRaw, sizeof(double)  * nx*ny*nz , cudaMemcpyDeviceToHost );
+
+  for(int ii=2;ii<5;ii++)
+  for(int jj=2;jj<5;jj++)
+  for(int kk=2;kk<5;kk++)
+  {
+     printf("(%d,%d,%d) = %f\n",ii,jj,kk,h_dVm[ kk + nz*(jj + ny*ii)]);
+  }
+  
   cudaFree(VmRaw);
   cudaFree(dVmRaw);
   cudaFree(sigmaRaw);
+  free(h_dVm);
+  free(h_sigma);
 }
