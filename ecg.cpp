9b115c1b64177fc5fdf390b7f90709520f3c45bd
Robert Blake <blake14@llnl.gov>
2018-04-30 10:50:19 -0700

    removing Dan's comments.

--- ecg.cpp
+++ ecg.cpp
@@ -1,39 +1,3 @@
-//                                MFEM Example 1
-//
-// Compile with: make ex1
-//
-// Sample runs:  ex1 -m ../data/square-disc.mesh
-//               ex1 -m ../data/star.mesh
-//               ex1 -m ../data/escher.mesh
-//               ex1 -m ../data/fichera.mesh
-//               ex1 -m ../data/square-disc-p2.vtk -o 2
-//               ex1 -m ../data/square-disc-p3.mesh -o 3
-//               ex1 -m ../data/square-disc-nurbs.mesh -o -1
-//               ex1 -m ../data/disc-nurbs.mesh -o -1
-//               ex1 -m ../data/pipe-nurbs.mesh -o -1
-//               ex1 -m ../data/star-surf.mesh
-//               ex1 -m ../data/square-disc-surf.mesh
-//               ex1 -m ../data/inline-segment.mesh
-//               ex1 -m ../data/amr-quad.mesh
-//               ex1 -m ../data/amr-hex.mesh
-//               ex1 -m ../data/fichera-amr.mesh
-//               ex1 -m ../data/mobius-strip.mesh
-//               ex1 -m ../data/mobius-strip.mesh -o -1 -sc
-//
-// Description:  This example code demonstrates the use of MFEM to define a
-//               simple finite element discretization of the Laplace problem
-//               -Delta u = 1 with homogeneous Dirichlet boundary conditions.
-//               Specifically, we discretize using a FE space of the specified
-//               order, or if order < 1 using an isoparametric/isogeometric
-//               space (i.e. quadratic for quadratic curvilinear mesh, NURBS for
-//               NURBS mesh, etc.)
-//
-//               The example highlights the use of mesh refinement, finite
-//               element grid functions, as well as linear and bilinear forms
-//               corresponding to the left-hand side and right-hand side of the
-//               discrete linear system. We also cover the explicit elimination
-//               of essential boundary conditions, static condensation, and the
-//               optional connection to the GLVis tool for visualization.
 
 #include "mfem.hpp"
 #include <fstream>
@@ -71,39 +35,13 @@ int main(int argc, char *argv[])
    }
    args.PrintOptions(cout);
 
-   // 2. Read the mesh from the given mesh file. We can handle triangular,
-   //    quadrilateral, tetrahedral, hexahedral, surface and volume meshes with
-   //    the same code.
    Mesh *mesh = new Mesh(mesh_file, 1, 1);
    int dim = mesh->Dimension();
 
-   // 3. Refine the mesh to increase the resolution. In this example we do
-   //    'ref_levels' of uniform refinement. We choose 'ref_levels' to be the
-   //    largest number that gives a final mesh with no more than 50,000
-   //    elements.
-   {
-      int ref_levels =
-         (int)floor(log(50000./mesh->GetNE())/log(2.)/dim);
-      for (int l = 0; l < ref_levels; l++)
-      {
-         mesh->UniformRefinement();
-      }
-   }
-
-   // 4. Define a finite element space on the mesh. Here we use continuous
-   //    Lagrange finite elements of the specified order. If order < 1, we
-   //    instead use an isoparametric/isogeometric space.
-   //    dk: order = no. polynomial basis functions per element (scaled by
-   //                           dimension)
-   //                e.g., 1D order 1 = 2 linear functions per element
-   //                      if 1D with a node in between: still order 1.
-   //        defining master element (unit sides, etc.)
    FiniteElementCollection *fec;
    if (order > 0)
    {
       fec = new H1_FECollection(order, dim);
-      // in our case: order = 1, dim = 3.  order is the degree of the basis
-      //                                   fct.
    }
    else if (mesh->GetNodes())
    {
@@ -114,12 +52,6 @@ int main(int argc, char *argv[])
    {
       fec = new H1_FECollection(order = 1, dim);
    }
-   // dk: vtk file, for example -- discretizes,  Also give it "master
-   //     element template.
-   // It comes up with no. unknowns, mapping between physical elements and
-   // master (how transformed the unit-side element is).
-   // one tet = 4 unknowns (one per vertex).  Two tetrahedrons face-to-face
-   // = 5.
    FiniteElementSpace *fespace = new FiniteElementSpace(mesh, fec);
    cout << "Number of finite element unknowns: "
         << fespace->GetTrueVSize() << endl;
@@ -129,9 +61,6 @@ int main(int argc, char *argv[])
    //    In this example, the boundary conditions are defined by marking all
    //    the boundary attributes from the mesh as essential (Dirichlet) and
    //    converting them to a list of true dofs.
-   // dk: mark faces.  Dirichlet = known concentration.  A Neumann flux
-   // condition is not setting a value; instead, add a term to system
-   // (later).
    Array<int> ess_tdof_list;   // Essential true degrees of freedom
                                // "true" takes into account shared vertices.
    if (mesh->bdr_attributes.Size())
@@ -144,8 +73,6 @@ int main(int argc, char *argv[])
    // 6. Set up the linear form b(.) which corresponds to the right-hand side of
    //    the FEM linear system, which in this case is (1,phi_i) where phi_i are
    //    the basis functions in the finite element fespace.
-   // dk: if had a forcing function (source term) on RHS.
-   //     In our there is none.  So replace these (later) in linear object.
    LinearForm *b = new LinearForm(fespace);
    ConstantCoefficient one(1.0);  // coef in front of grad u grad v
                                   // We'll change to tensor coefficient
@@ -157,9 +84,6 @@ int main(int argc, char *argv[])
    // 7. Define the solution vector x as a finite element grid function
    //    corresponding to fespace. Initialize x with initial guess of zero,
    //    which satisfies the boundary conditions.
-   // dk: grid fct is vector with extra info.  vec of length of no. unknowns
-   // in system -- extra info: map to physical space; connectivity/shared
-   // vertices; knows how to interpolate.
    GridFunction x(fespace);
    x = 0.0;   // essential boundary conditions are zero, so set whole thing
               // to zero.
@@ -167,12 +91,6 @@ int main(int argc, char *argv[])
    // 8. Set up the bilinear form a(.,.) on the finite element space
    //    corresponding to the Laplacian operator -Delta, by adding the Diffusion
    //    domain integrator.
-   // dk: setting up LHS.
-   //     "test function" "trial function" -- multiply A matrix by test and
-   //     trial function.  "Bi-linear" - linear in each of the terms.
-   //     "v" and "u".  (Not really "test" and "trial" because linear algebra
-   //     does it).
-   //     a is finite-element-object iwth instructions on how to form matrix.
    BilinearForm *a = new BilinearForm(fespace);   // defines a.
    // this is the Laplacian: grad u . grad v with linear coefficient.
    // we defined "one" ourselves in step 6.
@@ -182,10 +100,6 @@ int main(int argc, char *argv[])
    //    applying any necessary transformations such as: eliminating boundary
    //    conditions, applying conforming constraints for non-conforming AMR,
    //    static condensation, etc.
-   // dk: at this point, get out linear object (linear system)
-   //     this sets up the loops Rob talked about. When we set up the
-   //     domain integrator, above, it now knows to insert that in the
-   //     innermost loop.
    if (static_cond) { a->EnableStaticCondensation(); }
    a->Assemble();   // This creates the loops.
 
@@ -201,10 +115,6 @@ int main(int argc, char *argv[])
 #ifndef MFEM_USE_SUITESPARSE
    // 10. Define a simple symmetric Gauss-Seidel preconditioner and use it to
    //     solve the system A X = B with PCG.
-   // dk: can use either preconditioned conjugate gradient (simplest linear
-   //     solver); or UMPFACK - direct solver (gaussian elimination).
-   //     (PCG better for giant systems, sparse matrices.  HPC > 99% of
-   //     time use iterative method.)
    GSSmoother M(A);
    PCG(A, M, B, X, 1, 200, 1e-12, 0.0);
 #else
