e19f24d931e9440d6fcb92ac8287ef8333f7f4db
Robert Blake <blake14@llnl.gov>
2018-10-22 16:27:47 -0700

    Removing a bunch of dead code from the repo

--- src/OHaraRudy_Voltage.c
+++
@@ -1,5 +0,0 @@
-#include "OHaraRudy.h"
-#include "OHaraRudy_Voltage.h"
-void OHaraRudy_VoltageFunc(CELLPARMS *parmsPtr, double *state, int pOffset, DERIVED *derived, double dt )
-{
-}
--- src/null_INull.c
+++
@@ -1,7 +0,0 @@
-#include "OHaraRudy.h"
-#include "null_INull.h"
-
-void null_INullFunc(CELLPARMS *parmsPtr, double *state, int pOffset, DERIVED *derived, double dt)
-{
-
-}
