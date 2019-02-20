f1c0e5a678ae6b175b86adf7307dda4eea104fa6
Robert Blake <blake14@llnl.gov>
2019-02-20 09:18:14 -0800

    Adding a tool to bundle up all the hdf5 output.

---
+++ tools/femheartViz/hdf5bundle.py
@@ -0,0 +1,36 @@
+
+import numpy as np
+import h5py
+import re
+
+def main():
+    import argparse
+    import os
+    parser = argparse.ArgumentParser()
+    parser.add_argument("outdir", help="Directory used for femheart output")
+    args = parser.parse_args()
+    
+    outdir = args.outdir
+    outdir = os.path.realpath(outdir)
+    #Make HDF5 file
+    h5file = h5py.File(outdir+".h5", 'w')
+    #Scan output for timesteps
+    for possibleTime in os.listdir(outdir):
+        #for each timestep
+        timedir = os.path.join(outdir,possibleTime)
+        if (re.match(r"^timestep[0-9]+", possibleTime)
+            and
+            os.path.isdir(timedir)):
+            #scan directory for npy files
+            for filename in os.listdir(timedir):
+                m = re.match(r'^(.*)\.npy',filename)
+                if m:
+                    arrayName = m.group(1)
+                    arrayFile = os.path.join(timedir,filename)
+                    if possibleTime not in h5file:
+                        h5file.create_group(possibleTime)
+                    AAA = np.load(arrayFile)
+                    h5file[possibleTime][arrayName] = AAA
+
+if __name__=='__main__':
+    main()
